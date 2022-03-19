7378543727da643d3ebc519713370f7005fd2b07
Acrucha <mcaa@cin.ufpe.br>
2022-03-19 03:22:54 -0300

    Adição de página com tabela de cupons para restaurante e administrador

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -3,24 +3,29 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule }   from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarsComponent } from './cars/cars.component';
 import { CarService } from './cars/cars.service';
+import { TableComponent } from './views/table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarsComponent
+    CarsComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpModule,
+    MatTableModule,
     RouterModule.forRoot([
       {
         path: '',
@@ -32,13 +37,14 @@ import { CarService } from './cars/cars.service';
       },
       {
         path: 'restaurante',
-        component: CarsComponent
+        component: TableComponent
       },
       {
         path: 'adm',
-        component: CarsComponent
+        component: TableComponent
       }
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [CarService],
   bootstrap: [AppComponent]
