2cdb8e73cd3396e56ba314a0f846eb3515666f15
Alice Oliveira <aoqb@cin.ufpe.br>
2022-04-03 13:56:26 -0300

    feat: Componente history

---
+++ web-app/src/app/member/history/history.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "history",
+  templateUrl: "./history.component.html",
+  styleUrls: ["./history.component.css"],
+})
+export class HistoryComponent {}
