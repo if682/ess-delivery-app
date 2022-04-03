875ab09bbeb237211ce2f64d6da004764be2c9a5
Alice Oliveira <aoqb@cin.ufpe.br>
2022-04-03 13:57:43 -0300

    feat: Atualizando componente de registro

--- web-app/src/app/register/register.component.ts
+++ web-app/src/app/register/register.component.ts
@@ -1,33 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { NgModule } from '@angular/core';
-
-import { Client } from '../client/client';
-import { ClientService } from '../client/client.service';
+import { Component, OnInit } from "@angular/core";
+import { Client } from "src/app/client/client";
+import { ClientService } from "src/app/client/client.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
 export class RegisterComponent implements OnInit {
-   constructor(private clientService: ClientService) {}
+  constructor(private clientService: ClientService) {}
 
-   client: Client = new Client();
-   clients: Client[] = [];
+  client: Client = new Client();
+  clients: Client[] = [];
 
-   createClient(c: Client): void {
-      this.clientService.create(c)
-      .then(result => {
-            if (result) {
-               this.clients.push(<Client> result);
-               this.client = new Client();
-            }
-         })
-         .catch(erro => alert(erro));
-   }
+  createClient(c: Client): void {
+    this.clientService
+      .create(c)
+      .then((result) => {
+        if (result) {
+          this.clients.push(<Client>result);
+          this.client = new Client();
+        }
+      })
+      .catch((erro) => alert(erro));
+  }
 
-   ngOnInit(): void {
-      
-   }
-
+  ngOnInit(): void {}
 }
