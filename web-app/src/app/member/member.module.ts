c0458b36819928c338b38ef75d9aa90e9c972902
victoria <vlsc@cin.ufpe.br>
2022-04-09 20:58:22 -0300

    wip

--- web-app/src/app/member/member.module.ts
+++ web-app/src/app/member/member.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { MatButtonModule } from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
 import { RouterModule } from "@angular/router";
 import { HistoryComponent } from "./history/history.component";
 import { HomeComponent } from "./home/home.component";
