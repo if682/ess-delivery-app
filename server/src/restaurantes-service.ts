90ee4a63e590119965ddc99b063e60ffeb111297
Charles Matheus <cmsa@cin.ufpe.br>
2022-04-09 13:52:27 -0300

    Adicionando erros de formatação ao serviço de restaurante do servidor

--- server/src/restaurantes-service.ts
+++ server/src/restaurantes-service.ts
@@ -8,6 +8,22 @@ export class RestaurantesService {
     for (var key in restaurante)
       if (!restaurante[key])
         throw Error(`O campo ${key} não foi preenchido`);
+
+    if(!restaurante.cnpj.match(/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/))
+      throw Error('O campo de cnpj está mal formatado/incompleto');
+
+    if(!restaurante.cep.match(/^\d{5}\-\d{3}$/))
+      throw Error('O campo de cep está mal formatado/incompleto');
+
+    if(!restaurante.horario_inicio.match(/^\d{2}\:\d{2}$/))
+      throw Error('O campo de horario inicial está mal formatado/incompleto');
+
+    if(!restaurante.horario_fim.match(/^\d{2}\:\d{2}$/))
+      throw Error('O campo de horario final está mal formatado/incompleto');
+
+    if(!restaurante.telefone_responsavel.match(/^\(\d{2}\)\ \d{4,5}\-\d{4}$/))
+      throw Error('O canpo de telefone está mal formatado/incompleto');
+
     const newRestaurante = new Restaurante(<Restaurante> { ...restaurante });
     this.restaurantes.push(newRestaurante);
     return newRestaurante;
