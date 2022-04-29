72136514d7a88478e3c2027bff739493031649d3
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-29 10:18:32 -0300

    add(status-service): add a new status to the list addStatus(Status): Status

--- server/src/status-service.ts
+++ server/src/status-service.ts
@@ -5,4 +5,12 @@ export class Status_service {
     isUniqueID(targetID: number): boolean {
         return this.statusList.some(testID => testID.id == targetID);
     }
+    addStatus(order: Status): Status {
+    let status_state = new Status();
+     if(this.isUniqueID){
+        let status_state = order;
+         this.statusList.push(status_state);
+     }
+    return status_state;
+    }
 }
