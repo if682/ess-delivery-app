81f57dba96be273d6baf0e67b2969885c02fea2b
Enzo Bissoli <enzogurgel15@gmail.com>
2022-04-29 10:17:56 -0300

    add(status-service): check if statusID is unique isUniqueID(number): boolean

--- server/src/status-service.ts
+++ server/src/status-service.ts
@@ -2,4 +2,7 @@ import {Status} from "./status"
 
 export class Status_service {
     statusList: Status[] = [];
+    isUniqueID(targetID: number): boolean {
+        return this.statusList.some(testID => testID.id == targetID);
+    }
 }
