ee0f96182abb84720f028f4aa2cd14e4a0189aec
Enzo Bissoli <enzogurgel15@gmail.com>
2022-05-02 11:30:36 -0300

    add(constructor): to status.ts

--- web-app/src/app/status/status.ts
+++ web-app/src/app/status/status.ts
@@ -5,4 +5,10 @@ export class Status implements Pedido  {
     cnpj: string;
     id: number;
     statusVal: number = 0;
+    constructor(){
+      this.cpf = "";
+      this.cnpj = "";
+      this.id = -1;
+      this.statusVal = -1; // validation is implicit
+    }
 }
