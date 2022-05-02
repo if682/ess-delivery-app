3bd1ad2437ad5ad9a0daf38d4c2de68b2d2f9b60
Enzo Bissoli <enzogurgel15@gmail.com>
2022-05-02 11:30:01 -0300

    add(attributes): to status concrete class

--- web-app/src/app/status/status.ts
+++ web-app/src/app/status/status.ts
@@ -1,4 +1,8 @@
 import { Pedido } from './pedido';
 
 export class Status implements Pedido  {
+    cpf: string; // Suppose that order has being unpacked in cpf, cnpj and id
+    cnpj: string;
+    id: number;
+    statusVal: number = 0;
 }
