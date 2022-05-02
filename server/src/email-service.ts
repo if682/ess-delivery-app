402ff449560b7c4be068155e265e0468f8cde8a2
Pedro Cunha <pvcunh4@gmail.com>
2022-05-02 00:45:54 -0300

    envia email pelo botao da lista de pedidos realizados

--- server/src/email-service.ts
+++ server/src/email-service.ts
@@ -45,26 +45,25 @@ class EmailService {
         
         console.log(`sending email to: ${mail.to.email}`);
         const info = await this.transporter.sendMail({
-            from: 'group5@ess.com',
-            to: mail.to.email,
-            subject: mail.message.subject,
-            text: mail.message.body,
-            html: mail.message.body
-        });      
-        
-        if(info.rejected) {
+                        from: 'group5@ess.com',
+                        to: mail.to.email,
+                        subject: mail.message.subject,
+                        text: mail.message.body,
+                        html: mail.message.body}); 
+           
+        //console.log('>', info.response, info.rejected)
+        if(info.response != '250 OK') {
             this.isOn = false;
             this.waitingMails.push(mail);
         }
-
+        
         if(info.accepted) {
             console.log(`Message sent: ${info.response}`);
             this.isOn = true;
             if(this.waitingMails.length > 0) {
                 this.sendWaiting();
             }
-        }
-
+        } 
         return info;
     }
 }
