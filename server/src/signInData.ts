be889513c3c02c9016257c193d6ee3155ab553ad
leilanyaragao <leilany.ulisses@liferay.com>
2022-05-03 23:12:55 -0300

    POST_login

---
+++ server/src/signInData.ts
@@ -0,0 +1,19 @@
+export class SingInData{
+    private email: string;
+    private password: string;
+
+    constructor(email: string, password: string){
+        this.email = email;
+        this.password = password; 
+
+    }
+
+    getEmail(): string {
+        return this.email;
+    }
+
+    getPassword(): string {
+        return this.password;
+    }
+
+}
