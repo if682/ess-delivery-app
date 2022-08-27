89694a04ee3d6bf96188def3a20335a3ec14797a
Samuel Simões <samuelsimoes31@gmail.com>
2022-08-27 18:12:46 -0300

    feat: import models on controllers

--- back/src/controllers/albums.js
+++ back/src/controllers/albums.js
@@ -1,3 +1,4 @@
 import express from 'express';
+import { Album } from '../models/album';
 
 export const albumsRouter = express.Router();
--- back/src/controllers/songs.js
+++ back/src/controllers/songs.js
@@ -1,3 +1,5 @@
 import express from 'express';
 
+import { Song } from '../models/song';
+
 export const songsRouter = express.Router();
