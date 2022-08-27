4d0e8dd23a8393fdf6dafc48c7b5566eb999bb33
Samuel Simões <samuelsimoes31@gmail.com>
2022-08-27 17:50:39 -0300

    feat: verify if user already exista before create

--- back/src/controllers/artists.js
+++ back/src/controllers/artists.js
@@ -5,8 +5,15 @@ export const artistsRouter = express.Router();
 
 // register artist
 artistsRouter.post('', async (request, response) => {
+  const { email } = request.body;
+
   try {
+    if (await Artist.findOne({ email }))
+      return response.status(400).send({ error: 'User already exists' });
+
     const artist = await Artist.create(request.body);
+
+    artist.password = undefined;
 
     return response.status(201).send({ artist });
   } catch (error) {
