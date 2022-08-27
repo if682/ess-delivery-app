edd3b3e1dab1a9baddaa7c2eae6e317e82f8a6e3
Samuel Simões <samuelsimoes31@gmail.com>
2022-08-27 15:52:20 -0300

    feat: created artist model

---
+++ back/src/models/artist.js
@@ -0,0 +1,35 @@
+import mongoose, { mongo } from 'mongoose';
+
+const ArtistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  country: {
+    type: String,
+  },
+  genre: {
+    type: String,
+  },
+  image: {
+    data: Buffer,
+    contentType: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+export const Artist = mongoose.model('Artist', ArtistSchema);
