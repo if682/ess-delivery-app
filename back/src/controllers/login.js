2e7fb657118cb698a9c8d037275ddb4c754b3234
Samuel Simões <samuelsimoes31@gmail.com>
2022-08-30 15:59:20 -0300

    feat: add login route

--- back/src/controllers/login.js
+++ back/src/controllers/login.js
@@ -1,3 +1,23 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import { Artist } from '../models/artist.js';
 
 export const loginRouter = express.Router();
+
+loginRouter.post('', async (request, response) => {
+  const { email, password } = request.body;
+
+  const artist = await Artist.findOne({ email }).select('+password');
+
+  if (!artist) {
+    return response.status(400).send({ error: 'Invalid credentials' });
+  }
+
+  if (!await bcrypt.compare(password, artist.password)) {
+    return response.status(400).send({ error: 'Invalid credentials' });
+  }
+
+  artist.password = undefined;
+
+  response.send({ artist });
+});
