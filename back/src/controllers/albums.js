4827fe892f20f933f83552e2120fb3c6ee7100c7
Thallys <tcsr@cin.ufpe.br>
2022-09-07 17:37:52 -0300

    Criar album (sem relação)

--- back/src/controllers/albums.js
+++ back/src/controllers/albums.js
@@ -2,3 +2,15 @@ import express from 'express';
 import { Album } from '../models/album.js';
 
 export const albumsRouter = express.Router();
+
+albumsRouter.post('',async (request, response) => {
+    const {name,image} = request.body;
+    try {   
+        if(await Album.findOne({ name })) return response.status(400).send({ error: 'Album already exists' });
+        if(image&&!/\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(image)) return response.status(400).send({ error: 'Image format is not valid' });
+        const album = await Album.create(request.body);
+        return response.send({ album });
+    } catch (error) {
+        return response.status(400).send({ error: 'Register failed' });
+    }
+});
