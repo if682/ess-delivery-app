c928f74119ad50c4827c77b85f8f036c408a7044
ThallysRocha <tcsr@cin.ufpe.br>
2022-09-09 09:08:23 -0300

    feat: check if featuring artist is the song owner

--- back/src/controllers/songs.js
+++ back/src/controllers/songs.js
@@ -7,16 +7,16 @@ export const songsRouter = express.Router();
 songsRouter.post('',async (request, response) => {
     const {url,participations} = request.body;
     try {
-        let regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|\?v=)([^#&?]*).*/;
-        let match = url.match(regExp);
+        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=|\?v=)([^#&?]*).*/;
+        const match = url.match(regExp);
         if(match && match[2].length == 11){        
             if(await Song.findOne({ url })) return response.status(400).send({ error: 'Song already exists' });
-            if(!participations||await Artist.findOne({ 'name' : participations })){
+            const featuringArtist = await Artist.findOne({ 'name' : participations })
+            if(!participations||(featuringArtist._id != request.userId)){
                 const song = await Song.create(request.body);
-                return response.send({ song });
-                
+                return response.send({ song });                
             }
-            else return response.status(400).send({ error: 'Featuring artist does not exist'});
+            else return response.status(400).send({ error: 'Featuring artist is not valid'});
             
         }
         else return response.status(400).send({ error: 'Not a youtube URL' });         
