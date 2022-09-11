85f69aaf6046dbdabffb226e27b50f28e416fea9
Thallys <tcsr@cin.ufpe.br>
2022-09-11 19:47:07 -0300

    feat: checar se algum campo de música está vazio

--- back/src/controllers/albums.js
+++ back/src/controllers/albums.js
@@ -6,14 +6,16 @@ export const albumsRouter = express.Router();
 
 albumsRouter.post('',async (request, response) => {
     const {name,image,year,songs} = request.body;
-    try {
-        const userId = request.userId;
+
+    const userId = request.userId;
         const same = await Album.findOne({ name });   
         if(same && same.artist == userId) return response.status(400).send({ error: 'Album already exists' });
         if(!songs.length) return response.status(400).send({ error: 'A album needs a song' });
-        /*if(!songs.every((song)=>{
+        if(!songs.every((song)=>{
             return song.name && song.url;
-        })) return response.status(400).send({ error: 'A song is missing required an argument' });*/
+        })) return response.status(400).send({ error: 'A song is missing required an argument' });
+    
+    try {        
         const album = await Album.create({name, image, year, artist : userId});
         await Promise.all(songs.map(async song =>{
             const albumSong = new Song({...song, album: album._id}); 
