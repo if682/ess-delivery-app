6c49e0e4144779f2ad3198e8761025885ea71c64
Thallys <tcsr@cin.ufpe.br>
2022-09-14 18:49:59 -0300

    Fix: Ajuste no endereço do app

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
-import App from './pages/Home';
+import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
