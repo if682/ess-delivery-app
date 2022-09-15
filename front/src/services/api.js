1e022a3720d827d556da3eb653f998ffb6d87c6e
Samuel Simões <samuelsimoes31@gmail.com>
2022-09-14 21:57:24 -0300

    feat: add setAuthToken to api service

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,5 +1,13 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const api = axios.create({
   baseURL: "http://localhost:3333"
-})
+});
+
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
+  else
+    delete api.defaults.headers.common["Authorization"];
+};
