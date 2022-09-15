a425c3113354c9085219a5b3bc236fcbd386de36
Samuel Simões <samuelsimoes31@gmail.com>
2022-09-14 21:45:52 -0300

    feat: add inputs on login page to test login

--- front/src/pages/Login/index.jsx
+++ front/src/pages/Login/index.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react';
+import { useLogin } from '../../contexts/Login';
 
 const Login = () => {
-    return(
-        <div>
-            Login
-        </div>
-    );
+  const { handleLogin } = useLogin();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    handleLogin(email, password);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
+        <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
+        <button type="submit">AAAAAAAAAA</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
