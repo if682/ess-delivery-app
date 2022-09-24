e9c26c69f8ef632c8013b204378f7d3ca4954f79
Thallys Christ dos Santos Rocha <tcsr@cin.ufpe.br>
2022-09-24 12:37:34 -0300

    Fix: onClick dos componentes

--- front/src/components/AddAlbum/index.jsx
+++ front/src/components/AddAlbum/index.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import './styles.css'
 const AddAlbum = ({...props}) => {
-    const navigate = useNavigate();
     return(
-        <div className="AddAlbum" onClick={()=>navigate("/createAlbum")} {...props}>
+        <div className="AddAlbum" {...props}>
             <div className="Button-AddAlbum" ><div className="Cross">+</div></div>   
             <label className="AddAlbum-Label">Adicionar</label>    
         </div>
