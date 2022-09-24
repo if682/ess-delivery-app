6139c7e22900a4c5839d5ed5bdeb2efaabee8297
Samuel Simões <samuelsimoes31@gmail.com>
2022-09-24 20:48:32 -0300

    feat: hiding authenticated routes

--- front/src/pages/App.jsx
+++ front/src/pages/App.jsx
@@ -1,32 +1,12 @@
-import { BrowserRouter, Route, Routes, Redirect, Navigate } from "react-router-dom";
-import Artist from "./Artist";
-import Login from "./Login";
-import Register from "./Register";
-import EditArtist from "./EditArtist";
-import Album from "./Album";
-import CreateAlbum from "./CreateAlbum";
-import EditAlbum from "./EditAlbum";
-import CreateMusic from "./CreateMusic";
+import { BrowserRouter } from "react-router-dom";
 import AppProvider from '../contexts/index.jsx';
+import AppRoutes from '../routes/index.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <AppProvider>
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/artist" element={<Artist />} />
-          <Route path="/editArtist" element={<EditArtist />} />
-          <Route path="/album" element={<Album />} />
-          <Route path="/createAlbum" element={<CreateAlbum />} />
-          <Route path="/editAlbum" element={<EditAlbum />} />
-          <Route path="/createMusic" element={<CreateMusic />} />
-          <Route
-            path="*"
-            element={<Navigate to="/login" replace />}
-          />
-        </Routes>
+        <AppRoutes />
       </AppProvider>
     </BrowserRouter>
   );
---
+++ front/src/routes/index.jsx
@@ -0,0 +1,36 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Artist from "../pages/Artist";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import EditArtist from "../pages/EditArtist";
+import Album from "../pages/Album";
+import CreateAlbum from "../pages/CreateAlbum";
+import EditAlbum from "../pages/EditAlbum";
+import CreateMusic from "../pages/CreateMusic";
+import { useLogin } from '../contexts/Login';
+
+const AppRoutes = () => {
+  const { loggedUserId } = useLogin();
+  return (
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      {!!loggedUserId && (
+        <>
+          <Route path="/artist" element={<Artist />} />
+          <Route path="/editArtist" element={<EditArtist />} />
+          <Route path="/album" element={<Album />} />
+          <Route path="/createAlbum" element={<CreateAlbum />} />
+          <Route path="/editAlbum" element={<EditAlbum />} />
+          <Route path="/createMusic" element={<CreateMusic />} />
+        </>
+      )}
+      <Route
+        path="*"
+        element={<Navigate to="/login" replace />}
+      />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
