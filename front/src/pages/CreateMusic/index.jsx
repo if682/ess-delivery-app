f7646cde4b1c9fb0ac5a10a9505d3ad4bf4f9c67
ThallysRocha <tcsr@cin.ufpe.br>
2022-09-30 09:49:11 -0300

    refactor: onChange

--- front/src/pages/CreateMusic/index.jsx
+++ front/src/pages/CreateMusic/index.jsx
@@ -3,28 +3,17 @@ import Input from "../../components/Input"
 import Checkbox from "../../components/Checkbox"
 import Button from "../../components/Button"
 import React,{useState} from "react";
+import { useNavigate } from "react-router-dom";
+import { api, setAuthToken } from '../services/api';
 const CreateMusic = () => {
+    const navigate = useNavigate();
     const [name, setName] = useState(null);
     const [link, setLink] = useState(null);
     const [participation, setParticipation] = useState(null);
     const [explicit, setExplicit] = useState(false);
 
-    const handleInputChange = (e) => {
-        const {id , value} = e.target;
-        if(id==="name"){
-            setName(value);
-        }
-        if(id==="link"){
-            setLink(value);
-        }
-        if(id==="participation"){
-            setParticipation(value);
-        }
-        if(id==="explicit"){
-            setExplicit(value);
-        }
-    }
-    const handleSubmit = () => {
+    const onSubmit = (e) => {
+        e.preventDefault();
         if(name&&link){
             alert("ayooo");
         }
@@ -35,12 +24,12 @@ const CreateMusic = () => {
     return(
         <div className="CreateMusic">
             <h1 className="title">Cadastrar nova música</h1>
-            <form action="" className="form">
-            <Input placeholder="Nome" id="name" onChange = {(e) => handleInputChange(e)}>Nome da música*</Input>
-            <Input placeholder="Link" id="link" onChange = {(e) => handleInputChange(e)}>Link da música*</Input>
-            <Input placeholder="Participação" id="participation" onChange = {(e) => handleInputChange(e)}>Participação</Input>
-            <Checkbox id="explicit" >Explícito</Checkbox>
-            <Button onClick={()=>{handleSubmit()}}>Salvar</Button>
+            <form onSubmit={onSubmit} className="form">
+            <Input placeholder="Nome" id="name" onChange={e => setName(e.target.value)}>Nome da música*</Input>
+            <Input placeholder="Link" id="link" onChange={e => setLink(e.target.value)}>Link da música*</Input>
+            <Input placeholder="Participação" id="participation" onChange={e => setParticipation(e.target.value)}>Participação</Input>
+            <Checkbox id="explicit" onChange={e => setExplicit(e.target.value)}>Explícito</Checkbox>
+            <Button type="submit">Salvar</Button>
             </form>
 
         </div>
