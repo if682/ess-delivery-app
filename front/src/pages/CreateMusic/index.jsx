9a0c6b9afe46fb90909cb8ee289b644d0a21cd1a
ThallysRocha <tcsr@cin.ufpe.br>
2022-09-30 15:22:11 -0300

    add: post musica v1

--- front/src/pages/CreateMusic/index.jsx
+++ front/src/pages/CreateMusic/index.jsx
@@ -2,33 +2,41 @@ import "./styles.css"
 import Input from "../../components/Input"
 import Checkbox from "../../components/Checkbox"
 import Button from "../../components/Button"
-import React,{useState} from "react";
+import React,{useState, useCallback} from "react";
 import { useNavigate } from "react-router-dom";
-import { api, setAuthToken } from '../services/api';
+import { api} from '../../services/api';
 const CreateMusic = () => {
     const navigate = useNavigate();
     const [name, setName] = useState(null);
-    const [link, setLink] = useState(null);
-    const [participation, setParticipation] = useState(null);
-    const [explicit, setExplicit] = useState(false);
+    const [url, setUrl] = useState(null);
+    const [participations, setParticipations] = useState(null);
+    const [explicit, setExplicit] = useState(null);
 
-    const onSubmit = (e) => {
+    const onSubmit = async(e) => {
+        alert(explicit);
         e.preventDefault();
-        if(name&&link){
-            alert("ayooo");
+        if(name&&url){
+            const body = {name,url,participations,explicit};            
+            try {
+                const response = await api.post('/songs',body);
+                console.log(response);
+            } catch (error) {
+                alert("ayooo");
+            }
+            
         }
         else{
             alert("*Campo obrigatório não pode ser deixado vazio*");
         }
-    }
+    };
     return(
         <div className="CreateMusic">
             <h1 className="title">Cadastrar nova música</h1>
             <form onSubmit={onSubmit} className="form">
             <Input placeholder="Nome" id="name" onChange={e => setName(e.target.value)}>Nome da música*</Input>
-            <Input placeholder="Link" id="link" onChange={e => setLink(e.target.value)}>Link da música*</Input>
-            <Input placeholder="Participação" id="participation" onChange={e => setParticipation(e.target.value)}>Participação</Input>
-            <Checkbox id="explicit" onChange={e => setExplicit(e.target.value)}>Explícito</Checkbox>
+            <Input placeholder="Link" id="link" onChange={e => setUrl(e.target.value)}>Link da música*</Input>
+            <Input placeholder="Participação" id="participation" onChange={e => setParticipations(e.target.value)}>Participação</Input>
+            <Checkbox id="explicit" onClick={() => setExplicit(!explicit)}>Explícito</Checkbox>
             <Button type="submit">Salvar</Button>
             </form>
 
