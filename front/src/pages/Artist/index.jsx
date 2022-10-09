2a4f2c57870dd3d17c4fc9a8348a9de2df7b8c5b
Samuel Simões <samuelsimoes31@gmail.com>
2022-10-09 18:29:42 -0300

    feat: buttons to routes in artists page

--- front/src/pages/Artist/index.jsx
+++ front/src/pages/Artist/index.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 import { useLogin } from '../../contexts/Login';
 import { api } from '../../services/api';
+
+import Icon from '../../components/Icon'
+import AddAlbum from '../../components/AddAlbum'
+
 import "./styles.css"
+import { useNavigate } from 'react-router';
 
 const Artist = () => {
   const { loggedUserId, handleLogout } = useLogin();
   const [userData, setUserData] = useState();
+  const navigate = useNavigate()
 
   useEffect(() => {
     const getUserData = async () => {
@@ -23,7 +29,9 @@ const Artist = () => {
   return (
     <div>
       <p className='Nome'>{userData.name}</p>
-      <button onClick={() => handleLogout()}>sair</button>
+      <button className='IconButton' onClick={() => navigate('/editArtist')}><Icon iconType="Edit"/></button>
+      <button className='IconButton' onClick={() => handleLogout()}><Icon iconType="SignOut"/></button>
+      <AddAlbum onClick={() => navigate("/createAlbum")}/>
     </div>
   );
 };
