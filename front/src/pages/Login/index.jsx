7104eecc956fd7ea951dcccde13ad77b4cc0bef9
Samuel Simões <samuelsimoes31@gmail.com>
2022-10-09 17:20:49 -0300

    feat: login styles

--- front/src/pages/Login/index.jsx
+++ front/src/pages/Login/index.jsx
@@ -26,7 +26,7 @@ const Login = () => {
       <section className="Gradient"></section>
       <main className="LoginContainer">
         <form onSubmit={onSubmit} className='FormContainer'>
-          <p className="title">Login</p>
+          <p className="Title">Login</p>
           <p className="subtitle">
             {"Não é cadastrado? "}
             <Link className='Link' to='/register'>Faça o cadastro</Link>
