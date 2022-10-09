8847d3193a827d44c1846ff79515b2a31ddcca67
Samuel Simões <samuelsimoes31@gmail.com>
2022-10-09 17:54:43 -0300

    feat: fisnish register page styles

--- front/src/pages/Register/index.jsx
+++ front/src/pages/Register/index.jsx
@@ -1,20 +1,49 @@
 import Input from "../../components/Input";
 import Checkbox from "../../components/Checkbox";
+import LandingInfo from '../../components/LandingInfo';
+import "./styles.css"
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button';
 
 const Register = () => {
-    return(
-        <div>
-            <div>
-                <p>Cadastro</p>
-                <p>Já está cadastrado? Faça o login</p>
-            </div>
-            <Input children = {"Nome"}></Input>
-            <Input children = {"Email"}></Input>
-            <Input children = {"País"}></Input>
-            <Input children = {"Senha"}></Input>
-            <Checkbox></Checkbox>
-        </div>
-    );
+  return(
+    <div className='Container'>
+      <LandingInfo />
+      <main className="LoginContainer">
+        <form className='FormContainer'>
+          <p className='Title'>Cadastro</p>
+          <p className='subtitle'>
+            {"Já está cadastrado? "}
+            <Link className='Link' to='/login'>Faça o login</Link>
+          </p>
+          <Input
+            children={"Nome"}
+            placeholder="Nome"
+          />
+          <Input
+            children={"Email"}
+            placeholder="Email"
+          />
+          <Input
+            children={"País"}
+            placeholder="País"
+          />
+          <Input
+            children={"Gênero musical"}
+            placeholder="Gênero musical"
+          />
+          <Input
+            children={"Senha"}
+            placeholder="Senha"
+          />
+          <Checkbox>
+            Concordo com os termos e condições do  Esspotify
+          </Checkbox>
+          <Button type="submit">Cadastrar</Button>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default Register;
