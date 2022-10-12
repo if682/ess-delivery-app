2dfe854759e4dc9ab14a66711273f0ea290c4111
Samuel Simões <samuelsimoes31@gmail.com>
2022-10-12 16:13:30 -0300

    feat: navigation to edit album

--- front/src/pages/Song/index.jsx
+++ front/src/pages/Song/index.jsx
@@ -5,15 +5,17 @@ import React, { useState, useEffect } from "react";
 import { api } from '../../services/api';
 import Play from "../../components/PlayMusic";
 import { Routes, useNavigate } from "react-router";
+import Icon from '../../components/Icon';
 
 const Songs = () => {
   const navigate = useNavigate()
   const [songs, setSongs] = useState([]);
+  const [album, setAlbum] = useState({});
   const [playIndex, setPlayIndex] = useState(0);
   const params = useParams();
 
   useEffect(() => {
-    const getData = async () => {
+    const getSongs = async () => {
       try {
         const response = await api.get('/songs/fromAlbum/' + params.albumId);
         setSongs(response.data);
@@ -21,13 +23,34 @@ const Songs = () => {
         alert("error");
       }
     };
-    getData();
+    getSongs();
+    const getAlbumData = async () => {
+      try {
+        const response = await api.get('/albums/' + params.albumId);
+        setAlbum(response.data);
+      } catch (error) {
+        alert("error");
+      }
+    };
+    getAlbumData();
   }, []);
+
+  const handleNavigateEditAlbum = () => {
+    navigate('/editAlbum', ({
+      state: {
+        album,
+        songs
+      }
+    }))
+  }
 
   return (
     <div className="Song">
       <div className="Banner">
-        <h1>Album teste haha</h1>
+        <h1>{album.name ?? "Carregando..."}</h1>
+        <button className='IconButton' onClick={handleNavigateEditAlbum}>
+          <Icon iconType="Edit" />
+        </button>
         <button onClick={() => navigate(-1)}>voltar</button>
       </div>
       <div className="Songs">
