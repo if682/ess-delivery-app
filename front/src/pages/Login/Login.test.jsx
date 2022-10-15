2dd289dd6b8e88f3f622c890f56dd750cfc589d8
Samuel Simões <samuelsimoes31@gmail.com>
2022-10-15 18:13:35 -0300

    refactor: login test

--- front/src/pages/Login/Login.test.jsx
+++ front/src/pages/Login/Login.test.jsx
@@ -1,38 +1,28 @@
 import { cleanup, render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Login from '.';
-import { BrowserRouter } from 'react-router-dom';
-import AppProvider from '../../contexts';
 import { ContextsWrapper, mockedApi } from '../../tests/utils';
-import { api } from '../../services/api';
 
 afterEach(cleanup);
 
 describe('Login', () => {
 
-
-  test('Login mal sucedido', async () => {
+  test('Deve mostrar uma mensagem de alerta ao login ser mal sucedido', async () => {
 
     const alertMock = jest.spyOn(window, 'alert').mockImplementation();
 
     mockedApi.onPost("/auth/login").reply(400, { "error": "Invalid credentials" });
 
-    const { getAllByTestId, getByText } = render(
+    render(
       <ContextsWrapper>
-
         <Login />
-
       </ContextsWrapper>
     );
 
-    // await user.click(screen.getByRole('button'))
     fireEvent.click(screen.getByRole('button'));
-    // setTimeout(() => { expect(alertMock).toHaveBeenCalledTimes(1);},5000)
     await waitFor(() => {
       expect(alertMock).toHaveBeenCalledTimes(1);
     });
 
     expect(alertMock).toHaveBeenCalledWith('Erro ao fazer login!');
-    // const elemento = await screen.findByLabelText('Email')
-    // expect(elemento).toBeOnScreen()
   });
 });
