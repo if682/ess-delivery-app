cd28182e84c923ed46adab938983e0afe17a3d40
Samuel Simões <samuelsimoes31@gmail.com>
2022-10-16 00:01:25 -0300

    feat: refactor tests and add invalid login test

--- back/src/tests/auth.test.js
+++ back/src/tests/auth.test.js
@@ -1,31 +1,47 @@
-import mockingoose from 'mockingoose'
-import supertest from 'supertest'
+import mockingoose from 'mockingoose';
+import supertest from 'supertest';
 import { Artist } from '../models/artist';
 import bcrypt from 'bcrypt';
 import app from '../server';
 
-describe('auth', () => {
-  test("Should login when pass valid credentials", async () => {
-    const fakePassword = "123456"
-    const encryptedPassword = await bcrypt.hash(fakePassword, 10)
-
-    const _doc = {
+describe('AUTH', () => {
+  describe('LOGIN', () => {
+    const mockedArtist = {
       _id: '507f191e810c19729de860ea',
       name: 'Nome do cara',
       email: 'name@email.com',
-      password: encryptedPassword,
     };
+    const mockedArtistPassword = '123456'
 
-    mockingoose(Artist).toReturn(_doc, 'findOne')
-
-    const results = await supertest(app).post('/auth/login').send({
-      email: 'name@email.com',
-      password: fakePassword
+    beforeAll(async () => {
+      mockedArtist.password = await bcrypt.hash(mockedArtistPassword, 10);
+      mockingoose(Artist).toReturn(mockedArtist, 'findOne');
     })
 
-    expect(results.statusCode).toBe(200);
-    expect(results.body.token).toBeTruthy()
-    expect(results.body.artist.name).toBe('Nome do cara')
-  })
+    afterAll(() => {
+      mockingoose.resetAll();
+    });
 
+
+    test("Should login when pass valid credentials", async () => {
+      const results = await supertest(app).post('/auth/login').send({
+        email: mockedArtist.email,
+        password: mockedArtistPassword
+      });
+
+      expect(results.statusCode).toBe(200);
+      expect(results.body.token).toBeTruthy();
+      expect(results.body.artist.name).toBe(mockedArtist.name);
+    });
+
+    test("Login mal sucedido por senha inválida", async () => {
+      const results = await supertest(app).post('/auth/login').send({
+        email: mockedArtist.email,
+        password: "invalidpassword"
+      });
+
+      expect(results.statusCode).toBe(400);
+      expect(results.body.error).toBe("Invalid credentials");
+    });
+  });
 });
