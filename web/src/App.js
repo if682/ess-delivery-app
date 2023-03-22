dc318c61da7b99d64873d12561e3a4d328bd9f52
Maria Beatriz Bezerra <mbcb@cin.ufpe.br>
2023-03-22 19:43:49 -0300

    button

--- web/src/App.js
+++ web/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Cardapio from './components/Cardapio';
+import PrimaryButton from './components/atoms/primary-button/PrimaryButton'
 
 function App() {
   var itens = [{"flamengo":"arrascaeta"}, {"flamengo":"bittencourt"}]
 
   return (
-    <Cardapio/>
+    <div>
+      <PrimaryButton buttonContent="Clique aqui"/>
+      <p>aksdhkajshdkjsdksdjh</p>
+      <PrimaryButton buttonContent="Desativar"/>
+      <Cardapio/>
+    </div>
   );
 }
 
