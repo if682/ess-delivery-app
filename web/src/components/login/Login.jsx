981b4ebccbd0ad052129865e298126e95f1c0c41
Pedro <93168925+pedro-patriota@users.noreply.github.com>
2023-03-22 15:11:40 -0300

    making login front page

--- web/src/components/login/Login.jsx
+++ web/src/components/login/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import './Login.css'
 import { InputTextBox } from "./loginComponents/inputTextBox/InputText";
 import { RedButtonLogin } from "./loginComponents/RedButton/RedButton";
 import { InputCheckBox } from "./loginComponents/inputCheckBox/InputCheck";
+import Logo from "../../assets/logo.svg"
 import { WhiteButtonLogin } from "./loginComponents/whiteButton/WhiteButton";
 
 export const Login = () => {
@@ -9,14 +11,18 @@ export const Login = () => {
     const [check, setCheck] = useState(false)
     console.log(check)
     return (
-        <div className="App">
+        <div className="login-container">
+            <div className="login-box">
+                <img className="login-box-icon" src={Logo}></img>
+                <p className="login-box-title">Acesse a sua conta</p>
+                <InputTextBox set_val={setInput} placeholder={'Insira algo'}></InputTextBox>
+                <RedButtonLogin buttonText={'Entrar'}></RedButtonLogin>
+                <WhiteButtonLogin buttonText={'Entrar'}></WhiteButtonLogin>
+                <InputCheckBox checked={check} set_val={setCheck} defaultValue={false} textCheck={'Aceita os termos?'}></InputCheckBox>
+                <p>{input}</p>
+                <p>{check ? 'true' : 'false'}</p>
 
-            <InputTextBox set_val={setInput} placeholder={'Insira algo'}></InputTextBox>
-            <RedButtonLogin buttonText={'Entrar'}></RedButtonLogin>
-            <WhiteButtonLogin buttonText={'Entrar'}></WhiteButtonLogin>
-            <InputCheckBox checked={check} set_val={setCheck} defaultValue={false} textCheck={'Aceita os termos?'}></InputCheckBox>
-            <p>{input}</p>
-            <p>{check  ?  'true' : 'false'}</p>
+            </div>
         </div>
     );
 }
