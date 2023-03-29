1694e4e391c64e779da51b185bd26ed0eefea7b6
mmariabeatriz <mbcb@CESAR-LDAP.INTRANET>
2023-03-29 18:19:20 -0300

    creating routes

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout.js";
-import Login from "./pages/client/login/Login.js";
+import Login from "./pages/client-login/ClientLogin.js";
 
 export default function App() {
   return (
