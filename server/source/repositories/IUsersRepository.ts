52460e4016fda9f552ffba82fe40050553be3108
Matheus Batista <matheus.vbatista04@gmail.com>
2023-04-01 16:44:02 -0300

    feature(register-use-case): implement prisma users repository

--- server/source/repositories/IUsersRepository.ts
+++ server/source/repositories/IUsersRepository.ts
@@ -1,7 +1,7 @@
 import { Prisma, User } from "@prisma/client";
 
 export interface IUserRepository {
-    create(user: Prisma.UserCreateInput): Promise<User>;
+    create(data: Prisma.UserCreateInput): Promise<User>;
     findByUsername(username: string): Promise<User | null>;
     findById(id: string): Promise<User | null>;
 }
---
+++ server/source/repositories/prisma/PrismaUsersRepository.ts
@@ -0,0 +1,35 @@
+import { Prisma, User } from "@prisma/client";
+import { IUserRepository } from "../IUsersRepository";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export class PrismaUsersRepository implements IUserRepository {
+    async create(data: Prisma.UserCreateInput) {
+        const user = await prisma.user.create({
+            data,
+        })
+
+        return user;
+    }
+
+    async findByUsername(username: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                username
+            }
+        })
+
+        return user;
+    }
+    async findById(id: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        })
+
+        return user;
+    }
+
+}
