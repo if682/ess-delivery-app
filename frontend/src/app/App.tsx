2c88f60023a8bdf67cd68a5790cc3541ef636a1d
Ekistoclecio <95001613+Ekistoclecio@users.noreply.github.com>
2023-04-02 00:29:28 -0300

    feat: Update app component

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,23 +1,27 @@
-import './App.css';
-import Router from './routes';
-import AppBar from './components/AppBar';
-import AppTheme from './providers/Theme';
-import { SessionProvider } from './providers/SessionContext';
-import QueryProvider from './providers/useQuery';
+import "./App.css";
+import Router from "./routes";
+import AppBar from "./components/AppBar";
+import AppTheme from "./providers/Theme";
+import { SessionProvider } from "./providers/SessionContext";
+import QueryProvider from "./providers/useQuery";
+import SearchBar from "./components/SearchBar";
 
 function App() {
-
   return (
-    <div className='App'>
+    <div className="App">
       <QueryProvider>
         <SessionProvider>
           <AppTheme>
-            <AppBar/>
-            <Router/>
+            <AppBar
+              logged={false}
+              userName="Ekistoclecio Lima"
+              sideBar={false}
+            />
+            <SearchBar />
+            <Router />
           </AppTheme>
         </SessionProvider>
       </QueryProvider>
-      
     </div>
   );
 }
