fd758ae70216cba45e8a19e3c57e6870dc4fbab0
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-02 14:24:08 -0300

    Add route watchlist

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Profile from "../pages/Profile";
+import Watchlist from "../pages/Watchlist";
 
 const AppRoutes = () => {
     return(
         <Routes>
             <Route path="profile" element={<Profile />} />
+            <Route path="watchlist" element={<Watchlist />} />
         </Routes>
     );
 };
