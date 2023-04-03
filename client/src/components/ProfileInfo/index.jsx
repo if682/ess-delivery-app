3a6f5ed23e08b40ddc0ca2336908c977ac370097
Joao Medeiros <jpcm2@cin.ufpe.br>
2023-04-03 08:46:40 -0300

    Init profile info component

---
+++ client/src/components/ProfileInfo/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+const ProfileInfo = () => {
+
+  return (
+    <section className="profile-section">
+        
+        <div className="avatar-container">
+            <img src="../../assets/avatar-placeholder.svg" alt="Logo do Letterboxcin" />
+        </div>
+
+        <div className="name-description-container">
+
+        </div>
+
+        <div className="info-container">
+
+        </div>
+    </section>
+  );
+};
+
+export default ProfileInfo;
--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header";
+import ProfileInfo from "../../components/ProfileInfo";
 
 const Profile = () => {
   return (
     <>
       <Header />
+      <ProfileInfo />
     </>
   );
 }
