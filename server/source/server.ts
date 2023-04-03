0bfec5fe47203a5aac55358e6e6cf4d8d61ac35f
Matheus Batista <matheus.vbatista04@gmail.com>
2023-04-02 22:12:16 -0300

    feature(login): remove old login from main server file

--- server/source/server.ts
+++ server/source/server.ts
@@ -1,36 +1,5 @@
-import { z } from "zod";
 import { app } from "./app";
 import { env } from "./env";
-import { PrismaClient } from "@prisma/client";
-import { compare } from "bcryptjs";
-const prisma = new PrismaClient();
-
-app.post("/login", async (request, reply) => {
-    const loginBodySchema = z.object({
-        username: z.string(),
-        password: z.string(),
-    })
-
-    const { username, password } = loginBodySchema.parse(request.body);
-
-    const user = await prisma.user.findUnique({
-        where: {
-            username
-        }
-    })
-
-    if (!user) {
-        return reply.status(404).send("Bad request.")
-    }
-
-    const passwordIsMatch = await compare(user.password, password);
-
-    if (!passwordIsMatch) {
-        return reply.status(403).send("Unauthorized access.")
-    }
-
-    return reply.status(200).send(user);
-})
 
 app.listen({
     host: "0.0.0.0",
