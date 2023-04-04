8c3607bb950816c25f6af9e5d23dd774967fdeb2
Matheus Batista <matheus.vbatista04@gmail.com>
2023-04-04 01:46:49 -0300

    feature(list-user-reviews): add list reviews route

--- server/source/http/controllers/reviews/routes.ts
+++ server/source/http/controllers/reviews/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { createReview } from "./create-review";
+import { listReviews } from "./list-reviews";
 
 export async function reviewRoutes(app: FastifyInstance) {
     app.post("/review", createReview);
+    app.get("/review/:id", listReviews);
 }
