6623e4e5ae27a7e72f2c120270a79256b26713e6
Joao Medeiros <jpcm2@cin.ufpe.br>
2023-04-05 08:29:16 -0300

    feature(profileSections): Assets added

---
+++ client/src/components/ProfileSections/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+
+const ProfileSections = () => {
+
+  return (
+    <section className="profile-sections">
+        
+    </section>
+  );
+};
+
+export default ProfileSections;
