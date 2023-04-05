79e28e3328be64763c371588c3cd17033e2f6b10
ViniciusPrincipe <vbmp@cin.ufpe.br>
2023-04-05 13:02:51 -0300

    chore: added reservationResponse class to frontend

--- frontend/src/services/api/interfaces.ts
+++ frontend/src/services/api/interfaces.ts
@@ -21,3 +21,20 @@ export interface FormValues {
   bathrooms: number;
   photos: Array<File>;
 }
+
+export interface ReservationResponse{
+  id: string;
+  name: string; 
+  street:string;
+  streetNumber: number;
+  cep : string;
+  checkIn: string;
+  checkOut: string;
+  guests: number;
+  budget: number | '';
+  additionalInfo: string;
+  bedrooms: number;
+  beds: number;
+  bathrooms: number;
+  photos: Array<{url:string}>;
+}
