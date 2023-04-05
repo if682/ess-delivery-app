1218cba78ef4071d6597fd1ca77f0db17c2ab971
Joao Medeiros <jpcm2@cin.ufpe.br>
2023-04-05 08:39:36 -0300

    feature(profileSections): Button, imgs and Labes added

--- client/src/components/ProfileSections/index.jsx
+++ client/src/components/ProfileSections/index.jsx
@@ -6,7 +6,33 @@ const ProfileSections = () => {
 
   return (
     <section className="profile-sections">
+        <div className="films">
+          <button>
+            <img src="../../assets/movie.svg" alt=""/>
+            <span>Films</span>
+          </button>
+        </div>
+
+        <div className="reviews">
+          <button>
+            <img src="../../assets/review.svg" alt=""/>
+            <span>Reviews</span>
+          </button>
+        </div>
+
+        <div className="likes">
+          <button>
+            <img src="../../assets/like.svg" alt=""/>
+            <span>Likes</span>
+          </button>
+        </div>
         
+        <div className="watchlist">
+            <button>
+              <img src="../../assets/save.svg" alt=""/>
+              <span>Watchlist</span>
+            </button>
+        </div>
     </section>
   );
 };
