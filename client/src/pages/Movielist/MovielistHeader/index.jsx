85577798727a2c5dc3c6d398a363c6464479bb96
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-04 23:23:36 -0300

    Importar os handles de handleClick

--- client/src/pages/Movielist/MovielistHeader/index.jsx
+++ client/src/pages/Movielist/MovielistHeader/index.jsx
@@ -1,27 +1,14 @@
 import React from "react";
+import {
+  handleUserAvatarClick,
+  handleUserWatchedFilmsClick,
+  handleUserLikedFilmsClick,
+  handleUserReviewsClick,
+  handleUserMovielistsClick
+} from "../../handleClick.js";
 import "./styles.css";
 
 const MovielistHeader = ({ userAvatar, username, listName }) => {
-  const handleUserAvatarClick = () => {
-    alert(`Aqui deve ir para o perfil de ${username}`);
-  };
-
-  const handleWatchedFilmsClick = () => {
-    alert(`Aqui deve ir para os filmes assistidos de ${username}`);
-  };
-
-  const handleLikeClick = () => {
-    alert(`Aqui deve ir para os likes de ${username}`);
-  };
-
-  const handleReviewClick = () => {
-    alert(`Aqui deve ir para as reviews de ${username}`);
-  };
-
-  const handleMovielistClick = () => {
-    alert(`Aqui deve ir para as listas de filmes de ${username}`);
-  };
-
   const handleFilterByYearClick = () => {
     alert("Aqui deve filtrar os filmes por ano");
   };
@@ -38,20 +25,20 @@ const MovielistHeader = ({ userAvatar, username, listName }) => {
     <div className="movielist-header">
       <div className="listname-container">
           <div className="user-info">
-            <img className="user-avatar" src={userAvatar} alt="Avatar do usuário" onClick={handleUserAvatarClick} />
+            <img className="user-avatar" src={userAvatar} alt="Avatar do usuário" onClick={() => handleUserAvatarClick(username)} />
             <h1 className="user-name">{username}'s {listName}</h1>
           </div>
           <div className="buttons-container">
-            <button className="watched-films-button" onClick={handleWatchedFilmsClick} >
+            <button className="watched-films-button" onClick={() => handleUserWatchedFilmsClick(username)} >
             <img src="../../assets/films-icon.svg" alt="Ícone de filmes assistidos" />
             </button>
-            <button className="like-button" onClick={handleLikeClick}>
+            <button className="like-button" onClick={() => handleUserLikedFilmsClick(username)}>
             <img src="../../assets/like-icon.svg" alt="Ícone de like" />
             </button>
-            <button className="review-button" onClick={handleReviewClick}>
+            <button className="review-button" onClick={() => handleUserReviewsClick(username)}>
             <img src="../../assets/review-icon.svg" alt="Ícone de review" />
             </button>
-            <button className="movielist-button" onClick={handleMovielistClick}>
+            <button className="movielist-button" onClick={() => handleUserMovielistsClick(username)}>
             <img src="../../assets/movielist-icon.svg" alt="Ícone de movielist" />
             </button>
           </div>
