01326e18294af911c5db252a1585eb4c8b688426
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-05 01:22:36 -0300

    Movido sort/filter para Movielist

--- client/src/pages/Movielist/index.jsx
+++ client/src/pages/Movielist/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../../components/Header";
-import MovielistHeader from "./MovielistHeader";
+import MovielistHeader from "../../components/MovielistHeader";
 import Movie from "../../components/Movie";
 import "./styles.css";
 
@@ -17,10 +17,44 @@ const Movielist = () => {
 
   const listName = "Movielist";
 
+  const handleFilterByYearClick = () => {
+      alert("Aqui deve filtrar os filmes por ano");
+  };
+  
+  const handleFilterByGenreClick = () => {
+    alert("Aqui deve filtrar os filmes por gênero");
+  };
+  
+  const handleSortClick = () => {
+    alert("Aqui deve ordenar a lista de filmes");
+  };
+
   return (
     <div className="movielist-page">
       <Header />
       <MovielistHeader userAvatar="../../assets/profile-pic.svg" username="Mia Goth" listName={listName} />
+      
+      <div className="filter-sort-icons">
+          <button className="filter-year-button" onClick={handleFilterByYearClick}>
+            <div className="button-container">
+              <p>Filter by year</p>
+              <img src="../../assets/chev-down-icon.svg" alt="Ícone de filtro por ano" />
+            </div>
+          </button>
+          <button className="filter-genre-button" onClick={handleFilterByGenreClick}>
+            <div className="button-container">
+              <p>Filter by genre</p>
+              <img src="../../assets/chev-down-icon.svg" alt="Ícone de filtro por gênero" />
+            </div>
+          </button>
+          <button className="sort-button" onClick={handleSortClick}>
+            <div className="button-container">
+              <p>Sort by</p>
+              <img src="../../assets/sort-icon.svg" alt="Ícone de sort" />
+            </div>
+          </button>
+      </div>
+      
       <div className="movies-grid">
         {movies.map((movie, index) => (
           <Movie key={index} poster="../../assets/movie-poster.svg" title={movie.title} year={movie.year} />
