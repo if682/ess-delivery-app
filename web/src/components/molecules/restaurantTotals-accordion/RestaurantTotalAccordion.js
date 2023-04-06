f1fd2b5ff574c5a37e0c7f583d436719a8483ef1
“Ana <aca7@cin.ufpe.br>
2023-04-06 15:28:51 -0300

    Accordion de total por restaurante pronto - V1

--- web/src/components/molecules/restaurantTotals-accordion/RestaurantTotalAccordion.js
+++ web/src/components/molecules/restaurantTotals-accordion/RestaurantTotalAccordion.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./RestaurantTotalAccordion.css";
+import RedOutlineButton from "../../atoms/red-outline-button/RedOutlineButton";
 import Accordion from 'react-bootstrap/Accordion';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -11,15 +12,27 @@ function RestaurantTotalAccordion() {
       <Accordion.Item eventKey="0">
         <Accordion.Header>
           <Container>
-            <Row>
+            <Row className="align-items-center">
+              <Col sm={3}><img class="restImage" src="./sample.jpg" alt="Logo do Restaurante"/></Col>
               <Col sm><strong class="restaurantName">Restaurant1</strong></Col>
-              <Col sm><div>TotalOrdered:<strong class="totalOrdered"> R$10,00</strong>
-              </div></Col>
+              <Col sm><div>Total:<strong class="totalOrdered"> R$10,00</strong></div></Col>
             </Row>  
           </Container>
 		    </Accordion.Header>
         <Accordion.Body>
-          <img class="itemImage" src="./sample.jpg" alt="Item mais pedido"/>  MostOrderedName, SeeMoreButton (?)
+          <Container>
+            <Row className="align-items-center">
+              <Col sm={3}><img class="itemImage" src="./sample.jpg" alt="Item mais pedido"/></Col>
+              <Col sm><div class="mostDescription">Mais pedido:<br/><strong class="mostOrdered">BigMac<br/></strong>5 unidades</div></Col>
+              <Col sm={3}>
+                <RedOutlineButton
+                  chevron={false}
+                  width="10rem">
+                  Ver todos
+                </RedOutlineButton>
+              </Col>
+            </Row>  
+          </Container>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
