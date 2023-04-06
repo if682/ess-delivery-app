b770b968bf10fa2bb6101de9a0d1f2997d4ce221
“Ana <aca7@cin.ufpe.br>
2023-04-06 13:12:26 -0300

    Consertando route.js, merge do VSCode bugou

--- web/src/routes.js
+++ web/src/routes.js
@@ -3,13 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import MyAccountPage from "./pages/myaccount/Myaccount";
 import MyOrders from "./pages/myorders/Myorders";
-<<<<<<< HEAD
 import  Login from "./pages/client-login/ClientLogin";
 import ForgetPwd from "./pages/client-login/ForgetPwd/ForgetPwd";
 import { RestaurantMenu } from "./pages/restaurant-menu/RestaurantMenu";
-=======
 import OrderTotals from "./pages/client-orderTotals/OrderTotals";
->>>>>>> dev
 
 const UrlRoutes = () => {
   return (
@@ -20,11 +17,8 @@ const UrlRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/forgetPwd" element={<ForgetPwd />} />
         <Route path="/meus-pedidos" element={<MyOrders />} />
-<<<<<<< HEAD
         <Route path="/restaurantMenu" element={<RestaurantMenu/>} />
-=======
 		    <Route path="/total-pedidos" element={<OrderTotals />} />
->>>>>>> dev
       </Routes>
     </BrowserRouter>
   );
