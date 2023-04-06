ac10d39055d6746797eb5a43cb1ef205bd833de6
“Ana <aca7@cin.ufpe.br>
2023-04-06 12:42:35 -0300

    Implementando base da GUI de total pedido no mês

--- web/src/components/molecules/restaurantTotals-accordion/RestaurantTotalAccordion.js
+++ web/src/components/molecules/restaurantTotals-accordion/RestaurantTotalAccordion.js
@@ -1,16 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./RestaurantTotalAccordion.css";
 import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function RestaurantTotalAccordion() {
   return (
     <Accordion>
       <Accordion.Item eventKey="0">
         <Accordion.Header>
-		  <strong class="totalAccordionHeader">Restaurant1</strong> <div class="totalAccordionHeader">TotalOrdered: <strong> R$10,00</strong></div>
-		</Accordion.Header>
+          <Container>
+            <Row>
+              <Col sm><strong class="restaurantName">Restaurant1</strong></Col>
+              <Col sm><div>TotalOrdered:<strong class="totalOrdered"> R$10,00</strong>
+              </div></Col>
+            </Row>  
+          </Container>
+		    </Accordion.Header>
         <Accordion.Body>
-          MostOrderedImage, MostOrderedName, SeeMoreButton (?)
+          <img class="itemImage" src="./sample.jpg" alt="Item mais pedido"/>  MostOrderedName, SeeMoreButton (?)
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
--- web/src/pages/client-orderTotals/OrderTotals.jsx
+++ web/src/pages/client-orderTotals/OrderTotals.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PageTitle from "../../components/atoms/page-title/PageTitle";
 import "./OrderTotals.css";
+import RestaurantTotalAccordion from "../../components/molecules/restaurantTotals-accordion/RestaurantTotalAccordion";
 
 function OrderTotals() {
 
@@ -8,7 +9,7 @@ function OrderTotals() {
     <div className="my-orders-page-container position-relative d-flex flex-column align-items-center justify-content-center vh-100 p-4">
       <PageTitle>Total dos pedidos do mês</PageTitle>
 
-      <div> Add totals table here </div>
+      <div> <RestaurantTotalAccordion/> </div>
     </div>
   );
 }
