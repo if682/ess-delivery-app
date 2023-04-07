54dd72a68d5aecec17535da28ee4c07418c4780b
João Pedro <jpcm2@cin.ufpe.br>
2023-04-06 22:31:36 -0300

    feature(profileSections): Profile films section

---
+++ client/src/components/ProfileFilmsList/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Films from "./Films";
+import "./style.css";
+
+const ProfileFilmsList = () => {
+
+  return (
+    <article className="profile-films">
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+        <Films />
+    </article>
+  );
+};
+
+export default ProfileFilmsList;
