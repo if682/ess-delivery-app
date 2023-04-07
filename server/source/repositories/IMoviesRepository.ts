73179c6d1e854ad658c90046f13f9c2dc0258e77
pgoq <97684729+pgoq@users.noreply.github.com>
2023-04-07 17:17:15 -0300

    add criacao de listas Historico e Curtidos no registro do usuario

--- server/source/repositories/IMoviesRepository.ts
+++ server/source/repositories/IMoviesRepository.ts
@@ -3,7 +3,6 @@ import { Prisma, Movie, Evaluation } from "@prisma/client"
 
 export interface IMoviesRepository{
     createMovie(id: string): Promise<Movie>;
-    findMovie(id: string): Promise<Movie | null>;
     getAverage(id: string): Promise<number | null>;
     addEvaluation(userId: string, movieId: string, newRating: number): Promise<Evaluation>;
 }
--- server/source/use-cases/factories/make-register-usecase.ts
+++ server/source/use-cases/factories/make-register-usecase.ts
@@ -1,9 +1,12 @@
+import { PrismaListsRepository } from "../../repositories/prisma/PrismaListsRepository";
 import { PrismaUsersRepository } from "../../repositories/prisma/PrismaUsersRepository";
 import { RegisterUserUseCase } from "../register-user";
 
+
 export function makeRegisterUseCase() {
     const usersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUserUseCase(usersRepository);
+    const listsRepository = new PrismaListsRepository();
+    const registerUseCase = new RegisterUserUseCase(usersRepository, listsRepository);
 
     return registerUseCase;
 }
