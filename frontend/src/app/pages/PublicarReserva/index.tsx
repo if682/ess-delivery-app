da5044e1546c0ee90ec0d25a6057c6f52475f14b
Ekistoclecio <95001613+Ekistoclecio@users.noreply.github.com>
2023-04-08 19:40:37 -0300

    feat: Adjustments on the PublishReservation page

--- frontend/src/app/pages/PublicarReserva/index.tsx
+++ frontend/src/app/pages/PublicarReserva/index.tsx
@@ -1,20 +1,8 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import AppContainer from '../../components/Container';
-import ResponsiveAppBar from '../../components/AppBar';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Toolbar from '@mui/material/Toolbar';
-import ReservationForm from '../../components/FormularioReserva';
+import React from "react";
+import ReservationForm from "../../components/FormularioReserva";
 
 const ReservationPage: React.FC = () => {
-  return (
-    <>
-      <AppContainer>
-        <ReservationForm/>
-      </AppContainer>
-    </>
-  );
+  return <ReservationForm />;
 };
 
 export default ReservationPage;
