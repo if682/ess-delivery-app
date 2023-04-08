1cf997cc7558d628eb761d70c7588e21314e67fb
SarahLMelo <61260788+SarahLMelo@users.noreply.github.com>
2023-04-08 01:09:20 -0300

    Create Register Page

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,6 +1,15 @@
 import LoginBox from "../../components/Login";
 
 const Login = () => {
+  const wrongPassword = false;
+  const noPassword = false;
+  const usernameNotFound = false;
+  const toMuchTries = false;
+
+  function wrongPasswordAlert(){
+
+  }
+
   return (
     <>
       <LoginBox></LoginBox>
