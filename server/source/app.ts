17d28b8266c499b89226ed14bbd88c9ffff4814f
Matheus Batista <matheus.vbatista04@gmail.com>
2023-04-08 18:48:40 -0300

    fix(cors): add cors

--- server/source/app.ts
+++ server/source/app.ts
@@ -3,8 +3,15 @@ import { userRoutes } from "./http/controllers/users/routes";
 import { reviewRoutes } from "./http/controllers/reviews/routes";
 import { movieRoutes } from "./http/controllers/movies/routes";
 import { listRoutes } from "./http/controllers/lists/routes";
+import cors from "@fastify/cors";
 
 export const app = fastify();
+
+app.register(cors, {
+    origin: true,
+    methods: ["GET", "POST", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type"],
+});
 
 app.register(userRoutes);
 app.register(reviewRoutes);
