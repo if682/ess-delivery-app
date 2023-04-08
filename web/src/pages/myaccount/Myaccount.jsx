4ad703e9fd1e76952a4c4f1bb488969b8f4e9762
“Ana <aca7@cin.ufpe.br>
2023-04-08 18:46:38 -0300

    Adicionando botão de total de pedidos na pág de minha conta

--- web/src/pages/myaccount/Myaccount.jsx
+++ web/src/pages/myaccount/Myaccount.jsx
@@ -9,6 +9,7 @@ import "./Myaccount.css";
 function MyAccountPage() {
   const navigate = useNavigate();
   const goToMyOrders = () => navigate("/meus-pedidos");
+  const goToTotalOrders = () => navigate("/total-pedidos");
 
   return (
     <div className="my-account-page-container position-relative d-flex flex-column align-items-center justify-content-center vh-100 p-4">
@@ -16,6 +17,7 @@ function MyAccountPage() {
 
       <RedOutlineButton>Meus dados</RedOutlineButton>
       <RedOutlineButton onClick={goToMyOrders}>Meus pedidos</RedOutlineButton>
+      <RedOutlineButton onClick={goToTotalOrders}>Total pedidos</RedOutlineButton>
 
       <RedButton>Desativar conta</RedButton>
     </div>
