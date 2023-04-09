99619133121b9eb753e33687e5496a3a3afe3728
Matheus Batista <matheus.vbatista04@gmail.com>
2023-04-09 15:28:29 -0300

    feature(get-profile): add get profile to router

--- server/source/http/controllers/users/routes.ts
+++ server/source/http/controllers/users/routes.ts
@@ -4,6 +4,7 @@ import { authenticate } from "./authenticate";
 import { forgotpassword } from "./forgotpassword";
 import { changepassword } from "./changepassword";
 import { editUser } from "./edit-user";
+import { getProfile } from "./get-profile";
 
 export async function userRoutes(app: FastifyInstance) {
     app.post("/register", register);
@@ -11,4 +12,5 @@ export async function userRoutes(app: FastifyInstance) {
     app.put("/forgotpassword", forgotpassword);
     app.put("/changepassword", changepassword);
     app.put("/edit", editUser)
+    app.get("/profile/:id", getProfile);
 }
