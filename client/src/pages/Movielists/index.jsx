c3907aefb2c58da2f1ce17d1ea486185f2c9cbf3
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-09 03:16:30 -0300

    POST para criar lista + fix alguns erros

--- client/src/pages/Movielists/index.jsx
+++ client/src/pages/Movielists/index.jsx
@@ -6,36 +6,15 @@ import "./styles.css";
 
 // ID provisório do usuário para testes
 const userId = "e84ba398-64fe-4929-b9a6-1e065cbdca2b";
+const port = 4002;
 
 const Movielists = () => {
   const [lists, setLists] = useState([]);
   const [newListTitle, setNewListTitle] = useState("");
   const navigate = useNavigate();
 
-  let handleGetLists = async (e) =>{
-      e.preventDefault();
-      try{
-          let res = await fetch(`http://localhost:4002/list/${userId}`, {
-              method: "GET",
-              headers: {
-                  "Content-Type": "application/json"
-              }
-          });
-
-          if(res.ok){
-              console.log("Passou ok")
-              let data = await res.json();
-              setLists(data);
-          }else{
-            console.log("Ocorreu um erro no GET.")
-          }
-      }catch (err){
-          console.log(err);
-      }
-  };
-
-  const handleUserListClick = ({ title }) => {
-    alert(`Aqui deve ir para a página da lista ${title}`);
+  const handleUserListClick = ({ name }) => {
+    alert(`Aqui deve ir para a página da lista ${name}`);
     let path = `/profile/movielists/movielist`;
     navigate(path);
   };
@@ -44,7 +23,7 @@ const Movielists = () => {
     setNewListTitle(event.target.value);
   };
   
-  const handleCreateListClick = () => {
+  const handleCreateListClick = async () => {
     const forbiddenChars = ["&", "%", "$", "@"];
     const maxLength = 80;
 
@@ -61,39 +40,80 @@ const Movielists = () => {
     }
     
     else {
-      const existingList = lists.find((list) => list.title.toLowerCase() === newListTitle.trim().toLowerCase());
+      const existingList = lists.find((list) => list.name.toLowerCase() === newListTitle.trim().toLowerCase());
       
       if (existingList) {
         alert(`A lista "${newListTitle.trim()}" já existe!`);
       }
       
       else {
-        setLists([...lists, { title: newListTitle.trim() }]);
-        setNewListTitle("");
+        // faz uma requisição POST para a API para criar uma nova lista
+        try {
+          let response = await fetch(`http://localhost:${port}/list/${userId}`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              userId: userId,
+              name: newListTitle.trim(),
+            }),
+          });
+
+          if (response.ok) {
+            setLists([...lists, { name: newListTitle.trim(), userId: userId }]);
+            setNewListTitle("");
+            console.log("POST realizado com sucesso.");
+          } else {
+            console.log("Ocorreu um erro no POST.");
+          }
+        } catch (err) {
+          console.log(err);
+        }
       }
     }
   };
 
-  const handleDeleteListClick = (event, title) => {
+  const handleDeleteListClick = (event, name) => {
     // impede que o evento de clique na lista seja disparado
     event.stopPropagation();
     
     // exibe uma janela de diálogo perguntando ao usuário se ele realmente deseja excluir a lista
-    const userConfirmation = window.confirm(`Tem certeza que deseja excluir a lista "${title}"?`);
+    const userConfirmation = window.confirm(`Tem certeza que deseja excluir a lista "${name}"?`);
     
     if (userConfirmation) {
-      const newList = lists.filter((list) => list.title !== title);
+      const newList = lists.filter((list) => list.name !== name);
       setLists(newList);
     }
   };
 
   useEffect(() => {
+    console.log(lists);
     // atualiza a lista na interface sempre que o estado de listas for atualizado
   }, [lists]);
 
   useEffect(() => {
+    // faz uma requisição GET para a API para obter as listas do usuário
+    const handleGetLists = async () => {
+      try {
+        let response = await fetch(`http://localhost:${port}/list/${userId}`, {
+          method: "GET",
+        });
+  
+        if (response.ok) {
+          let data = await response.json();
+          setLists(Object.values(data).flat());
+          console.log("GET realizado com sucesso.");
+        } else {
+          console.log("Ocorreu um erro no GET.");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
     handleGetLists();
-  }, []);
+  }, []);  
   
   return (
     <div className="movielists-page">
@@ -111,20 +131,22 @@ const Movielists = () => {
           <button onClick={handleCreateListClick}>Create</button>
         </div>
 
-        {lists.length !== 0 ? (
+        {lists.length === 0 ? (<p>Loading...</p>) : (
           <ul>
-            {lists.map((list) => (
-              <li key={list.title} className="movielists-list-item" onClick={() => handleUserListClick(list)}>
-                <div className="list-name-container">
-                  <button className="delete-list" onClick={(event) => handleDeleteListClick(event, list.title)}>Delete</button>
-                  <span className="list-name">{list.title}</span>
-                </div>
-              </li>
-            ))}
+            {lists.filter(list => list.name !== "Curtidos" && list.name !== "Historico").map((list) =>
+                <li key={list.name} className="movielists-list-item" onClick={() => handleUserListClick(list)}>
+                  <div className="list-name-container">
+                    <button className="delete-list" onClick={(event) => handleDeleteListClick(event, list.name)}>Delete</button>
+                    <span className="list-name">{list.name}</span>
+                  </div>
+                </li>
+              )
+            }
           </ul>
-        ) : (
-          <p>This user does not have any lists.</p>
         )}
+
+        {/*Usuário só tem as listas Curtidos e Historico*/}
+        {lists.length === 2 && <p>This user does not have any lists.</p>}
 
       </div>
     </div>
