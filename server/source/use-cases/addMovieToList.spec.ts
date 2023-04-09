0b65b4aa90166943eeee993e1c3485c7ce27ac2e
pgoq <97684729+pgoq@users.noreply.github.com>
2023-04-09 15:36:27 -0300

    inMemory Repository for Review

--- server/source/use-cases/addMovieToList.spec.ts
+++ server/source/use-cases/addMovieToList.spec.ts
@@ -1,73 +1,68 @@
-import { PrismaListsRepository } from "../repositories/prisma/PrismaListsRepository";
-import { PrismaMoviesRepository } from "../repositories/prisma/PrismaMoviesRepository";
-import { PrismaUsersRepository } from "../repositories/prisma/PrismaUsersRepository";
-import { expect, describe, test, beforeEach, vi} from "vitest";
+import { expect, describe, test, beforeEach } from "vitest";
 import { AddMovieToListUseCase } from "./add-movie-to-list";
 import { RegisterUserUseCase } from "./register-user";
-import { PrismaClient } from "@prisma/client";
-import { Sql } from "@prisma/client/runtime";
+import { InMemoryListsRepository } from "../repositories/inMemory/inMemoryListsRepository";
+import { InMemoryMoviesRepository } from "../repositories/inMemory/inMemoryMoviesRepository";
+import { InMemoryUsersRepository } from "../repositories/inMemory/inMemoryUsersRepository";
 
-
-let listsRepository: PrismaListsRepository
-let moviesRepository: PrismaMoviesRepository
-let usersRepository: PrismaUsersRepository
+let inMemoryListsRepository: InMemoryListsRepository
+let inMemoryMoviesRepository: InMemoryMoviesRepository
+let inMemoryUsersRepository: InMemoryUsersRepository
 let sut: AddMovieToListUseCase
 let reg: RegisterUserUseCase
 
-const prisma = new PrismaClient()
-
 describe("Add movie to list use case", () => {
-    beforeEach(() => {
-        listsRepository = new PrismaListsRepository();
-        moviesRepository = new PrismaMoviesRepository();
-        usersRepository = new PrismaUsersRepository();
-        sut = new AddMovieToListUseCase(listsRepository, moviesRepository);
-        reg = new RegisterUserUseCase(usersRepository, listsRepository);
+    beforeEach(async () => {
+        inMemoryListsRepository = new InMemoryListsRepository();
+        inMemoryMoviesRepository = new InMemoryMoviesRepository();
+        inMemoryUsersRepository = new InMemoryUsersRepository();
+
+        sut = new AddMovieToListUseCase(inMemoryListsRepository, inMemoryMoviesRepository);
+        reg = new RegisterUserUseCase(inMemoryUsersRepository,inMemoryListsRepository);
+        
+        // cria usuário antes de cada teste
+        await reg.handle({
+            name: "Fulano",
+            email: "fulano@gmail.com",
+            username: "fulaninho123",
+            birthdate: new Date(),
+            password: "FulanoBox345!",
+            location: null,
+            phone: null
+        })
+        
     })
 
     test("should be able to add movie to default list", async () =>{
-
-        const user = await reg.handle({
-            name: "Fulano",
-            username: "FulanoLetter",
-            password: "Letterbox123!",
-            email: "fulanobox@cin.ufpe.br",
-            birthdate: new Date(),
-            phone: null,
-            location: null
-        })
-
-        await prisma.$executeRaw`DROP SCHEMA IF EXISTS public CASCADE;`
-        await prisma.$executeRaw `CREATE SCHEMA public`;
         
-
+        const user = await inMemoryUsersRepository.findByEmail("fulano@gmail.com");
         
+        if(!user){
+            console.log("erro procurando fulano");
+        }
+        else{
+            await sut.handle({
+                userId: user.id,
+                listName: "Historico",
+                movieId: "Carros",
+            })
 
+            await sut.handle({
+                userId: user.id,
+                listName: "Curtidos",
+                movieId: "Carros"
+            })
+
+            const seen = await inMemoryListsRepository.showMoviesFromList(user.id, "Historico")
+            const liked = await inMemoryListsRepository.showMoviesFromList(user.id, "Curtidos")
+            expect(seen.length).toEqual(1);
+            expect(liked.length).toEqual(1);
+        }
+
+    })
+
+    test("should not be able to add the same movie twice to a list", async() => {
         
-
-        listsRepository.createList(
-            "Historico",
-            "1109090"
-        )
-
-        const HistoryListSize = await sut.handle({
-            userId: "1109090",
-            listName: "Historico",
-            movieId: "1212"
-        })
-
-        // const LikedListSize = await sut.handle({
-        //     userId: "1109090",
-        //     listName: "Curtidos",
-        //     movieId: "1212"
-        // })
-
-        console.log(HistoryListSize);
-        // expect(HistoryListSize).toHaveLength(1);
-        //expect(LikedListSize).toHaveLength(1);
-
-
-
     })
 
 
