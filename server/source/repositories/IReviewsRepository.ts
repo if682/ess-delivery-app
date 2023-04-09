2a364e04b39e4d47bad70762694a4ac73a08d40a
Rayhene <rrda@poli.br>
2023-04-08 23:26:19 -0300

    feat: add list reviews from movie endpoint

--- server/source/repositories/IReviewsRepository.ts
+++ server/source/repositories/IReviewsRepository.ts
@@ -3,4 +3,5 @@ import { Prisma, Review } from "@prisma/client";
 export interface IReviewsRepository {
     create(data: Prisma.ReviewUncheckedCreateInput): Promise<Review>;
     findManyByAuthorId(id: string): Promise<Review[]>;
+    findManyByMovieId(id: string): Promise<Review[]>;
 }
