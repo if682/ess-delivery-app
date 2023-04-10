47e254089a09bb20b71e4cf9ee8434e6f48b8dae
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-04-10 01:25:08 -0300

    Add dropdown + pega listas do user

--- client/src/components/Movie/index.jsx
+++ client/src/components/Movie/index.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import HandleUserActions from "../../pages/handleUserActions.js";
 import "./styles.css";
+import { useState } from "react";
+
+const lists = JSON.parse(localStorage.getItem("userLists")).filter((list) => list.name !== "Curtidos" && list.name !== "Historico");
 
 const Movie = ({ poster, title, year }) => {
   const { handleAddToMovielistClick,
           handleLikeClick,
           handleMovieClick
         } = HandleUserActions();
+  
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
     <div className="movie-container">
       <div className="button-container">
-        <button className="add-to-movielist-button" onClick={() => handleAddToMovielistClick(title)}>
+        <button className="add-to-movielist-button" onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
           <img src="../../assets/add-list-icon.svg" alt="Ícone de adicionar a movielist" />
+          {isDropdownOpen && (
+            <div className="dropdown-container">
+              <p>Add to list:</p>
+              {lists.map((list) => (
+                <button key={list.name} onClick={() => handleAddToMovielistClick(title, list.name)}>
+                  {list.name}
+                </button>
+              ))}
+            </div>
+          )}
         </button>
 
         <button className="like-button" onClick={() => handleLikeClick(title)}>
@@ -26,7 +41,6 @@ const Movie = ({ poster, title, year }) => {
 
       <div className="info-container">
         <p className="title">{title}</p>
-        <p className="year">Year: {year}</p>
       </div>
     </div>
   );
