002e917a618f00ac2b3e801dab838611db2ad646
pgoq <97684729+pgoq@users.noreply.github.com>
2023-04-10 16:50:33 -0300

    fix make-addmovietolist-usecase and make-showlists-usecase

--- server/source/use-cases/factories/list/make-addmovietolist-usecase.ts
+++ server/source/use-cases/factories/list/make-addmovietolist-usecase.ts
@@ -1,11 +1,9 @@
 import { PrismaListsRepository } from "../../../repositories/prisma/PrismaListsRepository";
-import { PrismaMoviesRepository } from "../../../repositories/prisma/PrismaMoviesRepository";
 import { AddMovieToListUseCase } from "../../list/add-movie-to-list";
 
 export function makeAddMovieToListUseCase(){
     const listsRepository = new PrismaListsRepository();
-    const moviesRepository = new PrismaMoviesRepository();
-    const addmovietolistUseCase = new AddMovieToListUseCase(listsRepository, moviesRepository);
+    const addmovietolistUseCase = new AddMovieToListUseCase(listsRepository);
 
     return addmovietolistUseCase;
 }
--- server/source/use-cases/factories/list/make-showlists-usecase.ts
+++ server/source/use-cases/factories/list/make-showlists-usecase.ts
@@ -1,9 +1,11 @@
 import { PrismaListsRepository } from "../../../repositories/prisma/PrismaListsRepository";
+import { PrismaUsersRepository } from "../../../repositories/prisma/PrismaUsersRepository";
 import { ShowListsUseCase } from "../../list/show-lists";
 
 export function makeShowListsUseCase(){
     const listsRepository = new PrismaListsRepository();
-    const showlistsUseCase = new ShowListsUseCase(listsRepository);
+    const usersRepository = new PrismaUsersRepository();
+    const showlistsUseCase = new ShowListsUseCase(listsRepository, usersRepository);
 
     return showlistsUseCase;
 }
