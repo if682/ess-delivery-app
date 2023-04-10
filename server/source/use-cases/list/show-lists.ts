67b771a161020abf22d38c09643b6da5040353be
pgoq <97684729+pgoq@users.noreply.github.com>
2023-04-10 16:56:59 -0300

    add show lists use case tests

--- server/source/use-cases/list/show-lists.ts
+++ server/source/use-cases/list/show-lists.ts
@@ -1,5 +1,6 @@
 import { List } from "@prisma/client";
 import { IListsRepository } from "../../repositories/IListsRepository";
+import { IUserRepository } from "../../repositories/IUsersRepository";
 
 interface IShowListsUseCaseRequest{
     userId: string
@@ -10,11 +11,18 @@ interface IShowListsUseCaseReply{
 }
 
 export class ShowListsUseCase{
-    constructor(private listsRepository: IListsRepository){}
+    constructor(
+        private listsRepository: IListsRepository,
+        private usersRepository: IUserRepository
+        ){}
 
     async handle({
         userId
     }: IShowListsUseCaseRequest): Promise<IShowListsUseCaseReply>{
+        const foundUser = await this.usersRepository.findById(userId);
+        if(!foundUser){
+            throw new Error('Not Found');
+        }
         const lists = await this.listsRepository.showLists(userId);
 
         return {
