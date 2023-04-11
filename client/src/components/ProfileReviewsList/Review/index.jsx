71a107e013122d283f0675ae5280a58ad3d49b73
Rayhene <rrda@poli.br>
2023-04-11 17:33:30 -0300

    feat: add created time  and hide stars from reviews without rating

--- client/src/components/ProfileReviewsList/Review/index.jsx
+++ client/src/components/ProfileReviewsList/Review/index.jsx
@@ -4,6 +4,18 @@ import "./style.css";
 
 const Review = (props) => {
 
+
+  const createdAtDate = new Date(props.review.createdAt);
+
+  const day = createdAtDate.getDate();
+  const month = createdAtDate.getMonth() + 1;
+  const year = createdAtDate.getFullYear();
+  const hour = createdAtDate.getHours();
+  const minute = createdAtDate.getMinutes();
+  const second = createdAtDate.getSeconds();
+
+  const formattedCreatedAt = `${day}/${month}/${year} at ${hour}:${minute}:${second}`;
+
   return (
     <div className="review-container">
 
@@ -12,9 +24,14 @@ const Review = (props) => {
 
             <div className="review-title">
                 <h2>{props.review.title}</h2>
-                <img src="../../assets/star-icon.svg" />
-                <h3>{props.review.rating}/5</h3>
+                {props.review.rating >= 1 && 
+                  <>
+                    <img src="../../assets/star-icon.svg" alt="Rating star icon"/>
+                    <h3>{props.review.rating}/5</h3>
+                  </>
+                }
             </div>
+            <p>Logged review on {formattedCreatedAt}</p>
             <h2>{props.review.review}</h2>
         </div>
         
