416a26cedc8bd33608dc6373b0afa20f8a622029
João Pedro <jpcm2@cin.ufpe.br>
2023-04-10 23:04:29 -0300

    feature(edituser): Add password field, done

--- client/src/components/EditProfileForms/EditProfileFormsSection/index.jsx
+++ client/src/components/EditProfileForms/EditProfileFormsSection/index.jsx
@@ -1,19 +1,23 @@
 import { useState, useEffect } from "react";
 import "./style.css";
+import { useNavigate } from "react-router-dom";
+
 const EditProfileFormsSection = () => {
     const port = 4001
+    const navigate = useNavigate()
     const userId = localStorage.getItem("userId");
     const [name, setName] = useState("");
     const [userName, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [description, setDescription] = useState("");
+    const [password, setPassword] = useState("");
 
     const [userData, setUserData] = useState("");
 
     useEffect(() => {
         const handleGetUserData = async () => {
           try {
-            let response = await fetch(`http://localhost:${port}/edit`, {
+            let response = await fetch(`http://localhost:${port}/profile/${userId}`, {
               method: "GET",
             });
       
@@ -28,27 +32,40 @@ const EditProfileFormsSection = () => {
             console.log(err);
           }
         };
+
+        handleGetUserData();
     }, []);
 
     let handleSubmit = async (e) =>{
         e.preventDefault();
+        console.log(name)
+        console.log(userName)
+        console.log(description)
+        console.log(password)
+        console.log(userData.birthdate)
+        console.log(userData.phone)
+        console.log(userData.location)
         try{
-            let res = await fetch("https://httpbin.org/post", { //Esse link de post é um dummy apy
-                method: "POST",
+            let res = await fetch(`http://localhost:${port}/edit`, { //Esse link de post é um dummy apy
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json"
+                },
                 body: JSON.stringify({
                     name: name,
                     username: userName,
                     email: email,
                     description: description,
-                    password: userData.password,
+                    password: password,
                     birthdate: userData.birthdate,
                     phone: userData.phone,
                     location: userData.location
                 }),
             });
-
-            if(res.status === 200){
+            console.log(res.status)
+            if(res.status === 201){
                 console.log("Edit user deu certo, ok?")
+                navigate('/profile')
                 setName("");
                 setUsername("");
                 setEmail("");
@@ -71,7 +88,11 @@ const EditProfileFormsSection = () => {
             
             <div className="input-row">
                 <input className="first-input" type="text" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} required/>
-                <input className="second-input" type="text" value={description} placeholder="Description" onChange={(e) => setDescription(e.target.value)}/>
+                <input className="second-input" type="text" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+
+            <div className="input-row">
+                <input className="description-input" type="text" value={description} placeholder="Description" onChange={(e) => setDescription(e.target.value)}/>
             </div>
 
             <div className="submit-button-container">
