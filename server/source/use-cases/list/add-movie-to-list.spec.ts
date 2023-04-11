c377a7f033969da667e82a538973eea24496a925
pgoq <97684729+pgoq@users.noreply.github.com>
2023-04-11 17:18:15 -0300

    fix add movie to list use case

--- server/source/use-cases/list/add-movie-to-list.spec.ts
+++ server/source/use-cases/list/add-movie-to-list.spec.ts
@@ -3,9 +3,11 @@ import { AddMovieToListUseCase } from "./add-movie-to-list";
 import { RegisterUserUseCase } from "../user/register-user";
 import { InMemoryListsRepository } from "../../repositories/inMemory/inMemoryListsRepository";
 import { InMemoryUsersRepository } from "../../repositories/inMemory/inMemoryUsersRepository";
+import { InMemoryMoviesRepository } from "../../repositories/inMemory/inMemoryMoviesRepository";
 
 let inMemoryListsRepository: InMemoryListsRepository
 let inMemoryUsersRepository: InMemoryUsersRepository
+let inMemoryMoviesRepository: InMemoryMoviesRepository
 let sut: AddMovieToListUseCase
 let reg: RegisterUserUseCase
 let user_id: string
@@ -14,8 +16,9 @@ describe("Add movie to list use case", () => {
     beforeEach(async () => {
         inMemoryListsRepository = new InMemoryListsRepository();
         inMemoryUsersRepository = new InMemoryUsersRepository();
+        inMemoryMoviesRepository = new InMemoryMoviesRepository();
 
-        sut = new AddMovieToListUseCase(inMemoryListsRepository);
+        sut = new AddMovieToListUseCase(inMemoryListsRepository, inMemoryMoviesRepository);
         reg = new RegisterUserUseCase(inMemoryUsersRepository,inMemoryListsRepository);
         
         // cria usuário antes de cada teste
