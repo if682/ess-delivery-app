603fbfab26605c8049fe98144a29d225d7b729f9
João Pedro <jpcm2@cin.ufpe.br>
2023-04-11 15:14:05 -0300

    refact(Login+Register+EditUser-Tests): Describe added

--- client/src/pages/EditProfile/EditProfile.test.js
+++ client/src/pages/EditProfile/EditProfile.test.js
@@ -2,78 +2,80 @@ import { fireEvent, getByRole, render, screen, waitFor } from '@testing-library/
 import { BrowserRouter as Router } from 'react-router-dom';
 import EditProfileFormsSection from '../../components/EditProfileForms/EditProfileFormsSection'
 
-let nameField,usernameField, emailField, descriptionField, submitButton
-const mockLog = jest.spyOn(console, 'log');
+describe('Testes da tela de editar perfil', () => {
+    let nameField, usernameField, emailField, descriptionField, submitButton
+    const mockLog = jest.spyOn(console, 'log');
 
-beforeEach(() =>{
-    const { getByTestId } = render(
-        <Router>
-            <EditProfileFormsSection />
-        </Router>
-    );
-     nameField = getByTestId("name");
-     usernameField = getByTestId("username");
-     emailField = getByTestId("email");
-     descriptionField = getByTestId("description");
-     submitButton = getByTestId("submit");
-});
+    beforeEach(() => {
+        const { getByTestId } = render(
+            <Router>
+                <EditProfileFormsSection />
+            </Router>
+        );
+        nameField = getByTestId("name");
+        usernameField = getByTestId("username");
+        emailField = getByTestId("email");
+        descriptionField = getByTestId("description");
+        submitButton = getByTestId("submit");
+    });
 
-test('Editar profile foi bem sucedido', async () => {   
-    fireEvent.change(nameField, { target: { value: "jonh pedro" } });
-    fireEvent.change(usernameField, { target: { value: "john" } });
-    fireEvent.change(emailField, { target: { value: "john@email.com"}});
-    fireEvent.change(descriptionField,{ target: { value: "Este usuário ainda não possui descrição"}});
-    fireEvent.click(submitButton);
+    test('Editar profile foi bem sucedido', async () => {
+        fireEvent.change(nameField, { target: { value: "jonh pedro" } });
+        fireEvent.change(usernameField, { target: { value: "john" } });
+        fireEvent.change(emailField, { target: { value: "john@email.com" } });
+        fireEvent.change(descriptionField, { target: { value: "Este usuário ainda não possui descrição" } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(mockLog).toHaveBeenCalledWith("Edit profile foi enviado");
-      });
-});
+        await waitFor(() => {
+            expect(mockLog).toHaveBeenCalledWith("Edit profile foi enviado");
+        });
+    });
 
-test('Campo nome vazio, cadastro mal sucedido', async () => {
-    fireEvent.change(nameField, { target: { value: "" } });
-    fireEvent.change(usernameField, { target: { value: "john" } });
-    fireEvent.change(emailField, { target: { value: "john@email.com"}});
-    fireEvent.change(descriptionField,{ target: { value: "Este usuário ainda não possui descrição"}});
-    fireEvent.click(submitButton);
+    test('Campo nome vazio, cadastro mal sucedido', async () => {
+        fireEvent.change(nameField, { target: { value: "" } });
+        fireEvent.change(usernameField, { target: { value: "john" } });
+        fireEvent.change(emailField, { target: { value: "john@email.com" } });
+        fireEvent.change(descriptionField, { target: { value: "Este usuário ainda não possui descrição" } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(mockLog).toHaveBeenCalledWith("Campo nome vazio");
-      });
-});
+        await waitFor(() => {
+            expect(mockLog).toHaveBeenCalledWith("Campo nome vazio");
+        });
+    });
 
-test('Campo username vazio, cadastro mal sucedido', async () => {
-    fireEvent.change(nameField, { target: { value: "Jonh pedrro" } });
-    fireEvent.change(usernameField, { target: { value: "" } });
-    fireEvent.change(emailField, { target: { value: "john@email.com"}});
-    fireEvent.change(descriptionField,{ target: { value: "Este usuário ainda não possui descrição"}});
-    fireEvent.click(submitButton);
+    test('Campo username vazio, cadastro mal sucedido', async () => {
+        fireEvent.change(nameField, { target: { value: "Jonh pedrro" } });
+        fireEvent.change(usernameField, { target: { value: "" } });
+        fireEvent.change(emailField, { target: { value: "john@email.com" } });
+        fireEvent.change(descriptionField, { target: { value: "Este usuário ainda não possui descrição" } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(mockLog).toHaveBeenCalledWith("Campo username vazio");
-      });
-});
+        await waitFor(() => {
+            expect(mockLog).toHaveBeenCalledWith("Campo username vazio");
+        });
+    });
 
-test('Campo email vazio, cadastro mal sucedido', async () => {
-    fireEvent.change(nameField, { target: { value: "Jonh pedrro" } });
-    fireEvent.change(usernameField, { target: { value: "jpcm12312" } });
-    fireEvent.change(emailField, { target: { value: ""}});
-    fireEvent.change(descriptionField,{ target: { value: "Este usuário ainda não possui descrição"}});
-    fireEvent.click(submitButton);
+    test('Campo email vazio, cadastro mal sucedido', async () => {
+        fireEvent.change(nameField, { target: { value: "Jonh pedrro" } });
+        fireEvent.change(usernameField, { target: { value: "jpcm12312" } });
+        fireEvent.change(emailField, { target: { value: "" } });
+        fireEvent.change(descriptionField, { target: { value: "Este usuário ainda não possui descrição" } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(mockLog).toHaveBeenCalledWith("Campo email vazio");
-      });
-});
+        await waitFor(() => {
+            expect(mockLog).toHaveBeenCalledWith("Campo email vazio");
+        });
+    });
 
-test('Campo descrição vazio, cadastro mal sucedido', async () => {
-    fireEvent.change(nameField, { target: { value: "Jonh pedrro" } });
-    fireEvent.change(usernameField, { target: { value: "jpcm12312" } });
-    fireEvent.change(emailField, { target: { value: "Alo@email.com"}});
-    fireEvent.change(descriptionField,{ target: { value: ""}});
-    fireEvent.click(submitButton);
+    test('Campo descrição vazio, cadastro mal sucedido', async () => {
+        fireEvent.change(nameField, { target: { value: "Jonh pedrro" } });
+        fireEvent.change(usernameField, { target: { value: "jpcm12312" } });
+        fireEvent.change(emailField, { target: { value: "Alo@email.com" } });
+        fireEvent.change(descriptionField, { target: { value: "" } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(mockLog).toHaveBeenCalledWith("Campo descrição vazio");
-      });
-});
+        await waitFor(() => {
+            expect(mockLog).toHaveBeenCalledWith("Campo descrição vazio");
+        });
+    });
+})
--- client/src/pages/UserRegister/UserRegister.test.js
+++ client/src/pages/UserRegister/UserRegister.test.js
@@ -2,68 +2,70 @@ import { fireEvent, getByRole, render, screen, waitFor } from '@testing-library/
 import { BrowserRouter as Router } from 'react-router-dom';
 import UserRegister from '.'
 
-let nameField, usernameField, emailField, passwordField, birthdayField, phoneField, locationField, submitButton
-const mockLog = jest.spyOn(console, 'log');
+describe('Testes tela de registo', () => {
+    let nameField, usernameField, emailField, passwordField, birthdayField, phoneField, locationField, submitButton
+    const mockLog = jest.spyOn(console, 'log');
 
-beforeEach(() =>{
-    const { getByTestId } = render(
-        <Router>
-            <UserRegister />
-        </Router>
-    );
+    beforeEach(() => {
+        const { getByTestId } = render(
+            <Router>
+                <UserRegister />
+            </Router>
+        );
 
-    nameField = getByTestId("name");
-    usernameField = getByTestId("username");
-    emailField = getByTestId("email");
-    passwordField = getByTestId("password");
-    birthdayField = getByTestId("birthday");
-    phoneField = getByTestId("phone");
-    locationField = getByTestId("location");
-    submitButton = getByTestId("submit");
+        nameField = getByTestId("name");
+        usernameField = getByTestId("username");
+        emailField = getByTestId("email");
+        passwordField = getByTestId("password");
+        birthdayField = getByTestId("birthday");
+        phoneField = getByTestId("phone");
+        locationField = getByTestId("location");
+        submitButton = getByTestId("submit");
 
-});
+    });
 
-test('Testar se o registro foi bem sucedido', async () => {
-    fireEvent.change(nameField, { target: { value: "John Doe" } });
-    fireEvent.change(usernameField, { target: { value: "john1231231" } });
-    fireEvent.change(emailField, { target: { value: "john@email.com"}});
-    fireEvent.change(passwordField,{ target: { value: "Medeiros123!"}});
-    fireEvent.change(birthdayField, { target: { value: "2002-11-21"}});
-    fireEvent.change(phoneField, { target: { value: "1231231231"}});
-    fireEvent.change(locationField, { target: { value: "Recife"}});
-    fireEvent.click(submitButton);
+    test('Testar se o registro foi bem sucedido', async () => {
+        fireEvent.change(nameField, { target: { value: "John Doe" } });
+        fireEvent.change(usernameField, { target: { value: "john1231231" } });
+        fireEvent.change(emailField, { target: { value: "john@email.com" } });
+        fireEvent.change(passwordField, { target: { value: "Medeiros123!" } });
+        fireEvent.change(birthdayField, { target: { value: "2002-11-21" } });
+        fireEvent.change(phoneField, { target: { value: "1231231231" } });
+        fireEvent.change(locationField, { target: { value: "Recife" } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(mockLog).toHaveBeenCalledWith("Registro completo");
-      });
-});
+        await waitFor(() => {
+            expect(mockLog).toHaveBeenCalledWith("Registro completo");
+        });
+    });
 
-test('Testar campo nome vazio, registro mal sucedido', async () => {
-    fireEvent.change(nameField, { target: { value: "" } });
-    fireEvent.change(usernameField, { target: { value: "john1231231" } });
-    fireEvent.change(emailField, { target: { value: "john@email.com"}});
-    fireEvent.change(passwordField,{ target: { value: "Medeiros123!"}});
-    fireEvent.change(birthdayField, { target: { value: "2002-11-21"}});
-    fireEvent.change(phoneField, { target: { value: "1231231231"}});
-    fireEvent.change(locationField, { target: { value: "Recife"}});
-    fireEvent.click(submitButton);
+    test('Testar campo nome vazio, registro mal sucedido', async () => {
+        fireEvent.change(nameField, { target: { value: "" } });
+        fireEvent.change(usernameField, { target: { value: "john1231231" } });
+        fireEvent.change(emailField, { target: { value: "john@email.com" } });
+        fireEvent.change(passwordField, { target: { value: "Medeiros123!" } });
+        fireEvent.change(birthdayField, { target: { value: "2002-11-21" } });
+        fireEvent.change(phoneField, { target: { value: "1231231231" } });
+        fireEvent.change(locationField, { target: { value: "Recife" } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(mockLog).toHaveBeenCalledWith("Campo nome vazio");
-      });
-});
+        await waitFor(() => {
+            expect(mockLog).toHaveBeenCalledWith("Campo nome vazio");
+        });
+    });
 
-test('Testar campo senha vazio, registro mal sucedido', async () => {
-    fireEvent.change(nameField, { target: { value: "john pedro" } });
-    fireEvent.change(usernameField, { target: { value: "john1231231" } });
-    fireEvent.change(emailField, { target: { value: "john@email.com"}});
-    fireEvent.change(passwordField,{ target: { value: ""}});
-    fireEvent.change(birthdayField, { target: { value: "2002-11-21"}});
-    fireEvent.change(phoneField, { target: { value: "1231231231"}});
-    fireEvent.change(locationField, { target: { value: "Recife"}});
-    fireEvent.click(submitButton);
+    test('Testar campo senha vazio, registro mal sucedido', async () => {
+        fireEvent.change(nameField, { target: { value: "john pedro" } });
+        fireEvent.change(usernameField, { target: { value: "john1231231" } });
+        fireEvent.change(emailField, { target: { value: "john@email.com" } });
+        fireEvent.change(passwordField, { target: { value: "" } });
+        fireEvent.change(birthdayField, { target: { value: "2002-11-21" } });
+        fireEvent.change(phoneField, { target: { value: "1231231231" } });
+        fireEvent.change(locationField, { target: { value: "Recife" } });
+        fireEvent.click(submitButton);
 
-    await waitFor(() => {
-        expect(mockLog).toHaveBeenCalledWith("Campo senha vazio");
-      });
-});
+        await waitFor(() => {
+            expect(mockLog).toHaveBeenCalledWith("Campo senha vazio");
+        });
+    });
+})
