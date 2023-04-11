9f125563cc37e2b5c9032c4a358c2e5e0c365601
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2023-04-11 17:09:56 -0300

    Atualizar o EditPasswordPopup.test.js

--- web/src/pages/mydata/tests/EditPasswordPopup.test.js
+++ web/src/pages/mydata/tests/EditPasswordPopup.test.js
@@ -3,6 +3,8 @@ import { EditPasswordPopup } from "../components/EditPassword/EditPasswordPopup"
 
 describe("EditPasswordPopup", () => {
     let onHideMock, props;
+    
+    //mocka a função onHide e o props recebidos pelo popup
     beforeEach(() => {
         onHideMock = jest.fn();
         props = {
@@ -15,15 +17,21 @@ describe("EditPasswordPopup", () => {
         };
     });
     
+    //Verifica se o popup é renderizado corretamente 
     it('renders correctly', () => {
         const { container } = render(<EditPasswordPopup {...props} />);
-        expect(container.firstChild).toMatchSnapshot();
+
+//checa se o que foi renderizado corresponde ao snapshot do componente expect(container.firstChild).toMatchSnapshot();
     });
+
+//Verifica se a função mockada onHide é chamada quando se clica no botão de cancelar
     it('should call onHide when cancel button is clicked', () => {
         const { getByText } = render(<EditPasswordPopup {...props} />);
         fireEvent.click(getByText('Cancelar'));
         expect(onHideMock).toHaveBeenCalled();
       });
+
+//Verifica se o popup expõe a mensagem de erro "Preencha todos os campos!" se houver alguma caixa de texto vazia
     it('should display warning message "Preencha todos os campos!" if either password or oldPassword is null', async () => {
         const {getByPlaceholderText, getByText} = render(<EditPasswordPopup {...props} />);
         const oldPasswordField = getByPlaceholderText("Senha atual");
@@ -38,6 +46,7 @@ describe("EditPasswordPopup", () => {
         expect(warningMessage).toBeInTheDocument();
     });
 
+//Verifica se o popup expõe a mensagem de erro "O valor fornecido para a senha atual está incorreto" quando o valor recebido como senha não coincide com o valor da senha atual
     it('should display the warning message "O valor fornecido para a senha atual está incorreto" if oldPassword is incorrect', async () => {
         const {getByPlaceholderText, getByText} = render(<EditPasswordPopup {...props} />);
         const oldPasswordField = getByPlaceholderText("Senha atual");
@@ -54,6 +63,7 @@ describe("EditPasswordPopup", () => {
         expect(warningMessage).toBeInTheDocument();
     });
 
+    //Verifica se a função fetch é chamada para atualizar a senha do usuário quando todas as informações recebidas no input são válidas
     it("should call the fetch function to update the password when save button is clicked with correct inputs", async () => {
         global.fetch = jest.fn(() =>
         Promise.resolve({
