30f879e6913d96f0c59f46b1bd0fba2cf6a1f8b7
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2023-04-11 17:25:46 -0300

    Atualizar o DeactivateAccountPopup.test.js

--- web/src/pages/myaccount/deactivate-account-popup/DeactivateAccountPopup.test.js
+++ web/src/pages/myaccount/deactivate-account-popup/DeactivateAccountPopup.test.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 
 describe("DeactivateAccountPopup", () => {
     let onHideMock, props;
+    
+    //mocka a função onHide e cria o props que são recebidos pelo popup
     beforeEach(() => {
         onHideMock = jest.fn();
         props = {
@@ -12,15 +14,20 @@ describe("DeactivateAccountPopup", () => {
         };
     });
     
+    //Verifica se o popup é renderizado corretamente
     it('renders correctly', () => {
         const { container } = render(<BrowserRouter><DeactivateAccountPopup {...props} /></BrowserRouter>);
         expect(container.firstChild).toMatchSnapshot();
     });
+
+    //Verifica se a função mockada onHide é chamada quando o botão de cancelar é clicado 
     it('should call onHide when cancel button is clicked', () => {
         const { getByText } = render(<BrowserRouter><DeactivateAccountPopup {...props} /></BrowserRouter>);
         fireEvent.click(getByText('Cancelar'));
         expect(onHideMock).toHaveBeenCalled();
     });
+
+    //Verifica se o site expõe a tela de login após um clique no botão de confirmar
     it('should navigate to the login page when confirm button is clicked', () => {
         const { getByText } = render(<BrowserRouter><DeactivateAccountPopup {...props} /></BrowserRouter>);
         fireEvent.click(getByText('Confirmar'));
