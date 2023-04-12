68c528174fb286ae13188e64ed4a2a7e03473fcd
Matheus Batista <matheus.vbatista04@gmail.com>
2023-04-11 22:14:26 -0300

    chore(e2e): setup e2e prisma test environment

--- server/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ server/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,38 @@
+import "dotenv/config";
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+import { execSync } from "node:child_process";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+    if(!process.env.DATABASE_URL) {
+        throw new Error();
+    }
+
+    const url = new URL(process.env.DATABASE_URL);
+
+    url.searchParams.set("schema", schema);
+
+    return url.toString();
+}
 
 export default <Environment>{
     name: "prisma",
     async setup() {
-        console.log("OK")
-    
+        const schema = randomUUID();
+        const databaseURL = generateDatabaseURL(schema);
+
+        process.env.DATABASE_URL = databaseURL;
+        
+        execSync("npx prisma migrate deploy");
+
         return {
-            teardown(){}
+            async teardown(){
+                await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTIS "${schema}" CASCADE`)
+                await prisma.$disconnect()
+            }
         }
     }
-
 }
